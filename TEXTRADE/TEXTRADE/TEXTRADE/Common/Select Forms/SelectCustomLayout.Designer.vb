92f11245837aef456b58d1e1599be59c9c3aab01
Waseem Shaikh <sales@nakodainfotech.com>
2025-07-18 19:30:20 +0530

    LAYOUT WORK

---
+++ TEXTRADE/TEXTRADE/TEXTRADE/Common/Select Forms/SelectCustomLayout.Designer.vb
@@ -0,0 +1,250 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class SelectCustomLayout
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.BlendPanel1 = New VbPowerPack.BlendPanel()
+        Me.PBIMAGE1 = New System.Windows.Forms.PictureBox()
+        Me.gridbilldetails = New DevExpress.XtraGrid.GridControl()
+        Me.gridbill = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GCHK = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.CHKEDIT = New DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit()
+        Me.GUSERNAME = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.APPROXDATE = New DevExpress.XtraEditors.Repository.RepositoryItemDateEdit()
+        Me.CMDPHOTOVIEW = New System.Windows.Forms.Button()
+        Me.cmdok = New System.Windows.Forms.Button()
+        Me.cmdexit = New System.Windows.Forms.Button()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ExcelExport = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.TOOLREFRESH = New System.Windows.Forms.ToolStripButton()
+        Me.BlendPanel1.SuspendLayout()
+        CType(Me.PBIMAGE1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gridbilldetails, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gridbill, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CHKEDIT, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.APPROXDATE, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.APPROXDATE.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'BlendPanel1
+        '
+        Me.BlendPanel1.Blend = New VbPowerPack.BlendFill(VbPowerPack.BlendStyle.Vertical, System.Drawing.Color.FromArgb(CType(CType(213, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(248, Byte), Integer)), System.Drawing.SystemColors.Window)
+        Me.BlendPanel1.Controls.Add(Me.PBIMAGE1)
+        Me.BlendPanel1.Controls.Add(Me.gridbilldetails)
+        Me.BlendPanel1.Controls.Add(Me.CMDPHOTOVIEW)
+        Me.BlendPanel1.Controls.Add(Me.cmdok)
+        Me.BlendPanel1.Controls.Add(Me.cmdexit)
+        Me.BlendPanel1.Controls.Add(Me.ToolStrip1)
+        Me.BlendPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.BlendPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.BlendPanel1.Name = "BlendPanel1"
+        Me.BlendPanel1.Size = New System.Drawing.Size(1234, 581)
+        Me.BlendPanel1.TabIndex = 5
+        '
+        'PBIMAGE1
+        '
+        Me.PBIMAGE1.BackColor = System.Drawing.Color.White
+        Me.PBIMAGE1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PBIMAGE1.ErrorImage = Nothing
+        Me.PBIMAGE1.InitialImage = Nothing
+        Me.PBIMAGE1.Location = New System.Drawing.Point(382, 541)
+        Me.PBIMAGE1.Name = "PBIMAGE1"
+        Me.PBIMAGE1.Size = New System.Drawing.Size(28, 29)
+        Me.PBIMAGE1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PBIMAGE1.TabIndex = 481
+        Me.PBIMAGE1.TabStop = False
+        Me.PBIMAGE1.Visible = False
+        '
+        'gridbilldetails
+        '
+        Me.gridbilldetails.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gridbilldetails.Location = New System.Drawing.Point(17, 45)
+        Me.gridbilldetails.LookAndFeel.UseDefaultLookAndFeel = False
+        Me.gridbilldetails.MainView = Me.gridbill
+        Me.gridbilldetails.Name = "gridbilldetails"
+        Me.gridbilldetails.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.CHKEDIT, Me.APPROXDATE})
+        Me.gridbilldetails.Size = New System.Drawing.Size(319, 490)
+        Me.gridbilldetails.TabIndex = 431
+        Me.gridbilldetails.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gridbill})
+        '
+        'gridbill
+        '
+        Me.gridbill.Appearance.Row.Font = New System.Drawing.Font("Calibri", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gridbill.Appearance.Row.Options.UseFont = True
+        Me.gridbill.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GCHK, Me.GUSERNAME})
+        Me.gridbill.GridControl = Me.gridbilldetails
+        Me.gridbill.Name = "gridbill"
+        Me.gridbill.OptionsBehavior.AllowIncrementalSearch = True
+        Me.gridbill.OptionsCustomization.AllowColumnMoving = False
+        Me.gridbill.OptionsCustomization.AllowGroup = False
+        Me.gridbill.OptionsCustomization.AllowQuickHideColumns = False
+        Me.gridbill.OptionsView.ColumnAutoWidth = False
+        Me.gridbill.OptionsView.ShowAutoFilterRow = True
+        Me.gridbill.OptionsView.ShowFooter = True
+        Me.gridbill.OptionsView.ShowGroupPanel = False
+        '
+        'GCHK
+        '
+        Me.GCHK.ColumnEdit = Me.CHKEDIT
+        Me.GCHK.FieldName = "CHK"
+        Me.GCHK.Name = "GCHK"
+        Me.GCHK.OptionsColumn.ShowCaption = False
+        Me.GCHK.Visible = True
+        Me.GCHK.VisibleIndex = 0
+        Me.GCHK.Width = 30
+        '
+        'CHKEDIT
+        '
+        Me.CHKEDIT.AutoHeight = False
+        Me.CHKEDIT.Name = "CHKEDIT"
+        Me.CHKEDIT.NullStyle = DevExpress.XtraEditors.Controls.StyleIndeterminate.Unchecked
+        '
+        'GUSERNAME
+        '
+        Me.GUSERNAME.Caption = "User Name"
+        Me.GUSERNAME.FieldName = "USERNAME"
+        Me.GUSERNAME.Name = "GUSERNAME"
+        Me.GUSERNAME.OptionsColumn.AllowEdit = False
+        Me.GUSERNAME.Visible = True
+        Me.GUSERNAME.VisibleIndex = 1
+        Me.GUSERNAME.Width = 250
+        '
+        'APPROXDATE
+        '
+        Me.APPROXDATE.AutoHeight = False
+        Me.APPROXDATE.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.APPROXDATE.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton()})
+        Me.APPROXDATE.Name = "APPROXDATE"
+        '
+        'CMDPHOTOVIEW
+        '
+        Me.CMDPHOTOVIEW.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CMDPHOTOVIEW.Location = New System.Drawing.Point(416, 541)
+        Me.CMDPHOTOVIEW.Name = "CMDPHOTOVIEW"
+        Me.CMDPHOTOVIEW.Size = New System.Drawing.Size(80, 28)
+        Me.CMDPHOTOVIEW.TabIndex = 480
+        Me.CMDPHOTOVIEW.Text = "&View"
+        Me.CMDPHOTOVIEW.UseVisualStyleBackColor = True
+        '
+        'cmdok
+        '
+        Me.cmdok.BackColor = System.Drawing.Color.Transparent
+        Me.cmdok.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.cmdok.FlatAppearance.BorderSize = 0
+        Me.cmdok.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdok.ForeColor = System.Drawing.Color.Black
+        Me.cmdok.Location = New System.Drawing.Point(502, 541)
+        Me.cmdok.Name = "cmdok"
+        Me.cmdok.Size = New System.Drawing.Size(80, 28)
+        Me.cmdok.TabIndex = 5
+        Me.cmdok.Text = "&Ok"
+        Me.cmdok.UseVisualStyleBackColor = False
+        '
+        'cmdexit
+        '
+        Me.cmdexit.BackColor = System.Drawing.Color.Transparent
+        Me.cmdexit.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.cmdexit.FlatAppearance.BorderSize = 0
+        Me.cmdexit.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdexit.ForeColor = System.Drawing.Color.Black
+        Me.cmdexit.Location = New System.Drawing.Point(588, 541)
+        Me.cmdexit.Name = "cmdexit"
+        Me.cmdexit.Size = New System.Drawing.Size(80, 28)
+        Me.cmdexit.TabIndex = 6
+        Me.cmdexit.Text = "E&xit"
+        Me.cmdexit.UseVisualStyleBackColor = False
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExcelExport, Me.ToolStripSeparator1, Me.TOOLREFRESH})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(1234, 25)
+        Me.ToolStrip1.TabIndex = 430
+        Me.ToolStrip1.Text = "v"
+        '
+        'ExcelExport
+        '
+        Me.ExcelExport.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ExcelExport.Image = Global.TEXTRADE.My.Resources.Resources.Excel_icon
+        Me.ExcelExport.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ExcelExport.Name = "ExcelExport"
+        Me.ExcelExport.Size = New System.Drawing.Size(23, 22)
+        Me.ExcelExport.Text = "&Export to Excel"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'TOOLREFRESH
+        '
+        Me.TOOLREFRESH.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.TOOLREFRESH.Image = Global.TEXTRADE.My.Resources.Resources.refresh1
+        Me.TOOLREFRESH.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.TOOLREFRESH.Name = "TOOLREFRESH"
+        Me.TOOLREFRESH.Size = New System.Drawing.Size(23, 22)
+        Me.TOOLREFRESH.Text = "ToolStripButton1"
+        '
+        'SelectCustomLayout
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(1234, 581)
+        Me.Controls.Add(Me.BlendPanel1)
+        Me.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.KeyPreview = True
+        Me.Name = "SelectCustomLayout"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "Select Custom Layout"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.BlendPanel1.ResumeLayout(False)
+        Me.BlendPanel1.PerformLayout()
+        CType(Me.PBIMAGE1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gridbilldetails, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gridbill, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CHKEDIT, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.APPROXDATE.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.APPROXDATE, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BlendPanel1 As VbPowerPack.BlendPanel
+    Friend WithEvents PBIMAGE1 As PictureBox
+    Private WithEvents gridbilldetails As DevExpress.XtraGrid.GridControl
+    Private WithEvents gridbill As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GCHK As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents CHKEDIT As DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit
+    Friend WithEvents GUSERNAME As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents APPROXDATE As DevExpress.XtraEditors.Repository.RepositoryItemDateEdit
+    Friend WithEvents CMDPHOTOVIEW As Button
+    Friend WithEvents cmdok As Button
+    Friend WithEvents cmdexit As Button
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ExcelExport As ToolStripButton
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents TOOLREFRESH As ToolStripButton
+End Class
---
+++ TEXTRADE/TEXTRADE/TEXTRADE/Common/Select Forms/SelectCustomLayout.vb
@@ -0,0 +1,28 @@
+﻿Imports BL
+Public Class SelectCustomLayout
+    Private Sub SelectCustomLayout_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            FILLGRID()
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+    Sub FILLGRID()
+        Try
+            Dim OBJCMN As New ClsCommon
+            Dim DT As New DataTable
+
+            'Dim DTUSERS As DataTable = OBJCMN.SEARCH("USER_NAME", "", "USERMASTER", "AND User_yearid=" & YearId & " ")
+
+            DT = OBJCMN.SEARCH(" CAST(0 AS BIT) AS CHK,USER_NAME AS USERNAME", "", "USERMASTER", " AND User_yearid=" & YearId)
+
+            gridbilldetails.DataSource = DT
+            If DT.Rows.Count > 0 Then
+                gridbill.FocusedRowHandle = gridbill.RowCount - 1
+                gridbill.TopRowIndex = gridbill.RowCount - 15
+            End If
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+End Class
