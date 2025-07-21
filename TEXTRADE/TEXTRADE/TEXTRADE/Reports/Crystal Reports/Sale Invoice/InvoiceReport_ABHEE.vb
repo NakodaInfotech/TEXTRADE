c0d90728c9af5bbc07fc83411d53ac149e741f9a
Moin <sales@nakodainfotech.com>
2025-07-21 19:07:29 +0530

    Sale Invoice abhee reprot

--- TEXTRADE/TEXTRADE/TEXTRADE/Reports/Crystal Reports/Sale Invoice/InvoiceReport_ABHEE.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Reports/Crystal Reports/Sale Invoice/InvoiceReport_ABHEE.vb
@@ -395,30 +395,6 @@ Public Class InvoiceReport_ABHEE
             Return Me.ReportDefinition.Sections(42)
         End Get
     End Property
-    
-    <Browsable(false),  _
-     DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
-    Public ReadOnly Property Parameter_INVOICEREORT_TOTALLEFT_CHGS_Pm_INVOICEMASTERINVOICE_NO() As CrystalDecisions.[Shared].IParameterField
-        Get
-            Return Me.DataDefinition.ParameterFields(0)
-        End Get
-    End Property
-    
-    <Browsable(false),  _
-     DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
-    Public ReadOnly Property Parameter_INVOICEREORT_TOTALLEFT_CHGS_Pm_INVOICEMASTERINVOICE_REGISTERID() As CrystalDecisions.[Shared].IParameterField
-        Get
-            Return Me.DataDefinition.ParameterFields(1)
-        End Get
-    End Property
-    
-    <Browsable(false),  _
-     DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
-    Public ReadOnly Property Parameter_INVOICEREORT_TOTALLEFT_CHGS_Pm_INVOICEMASTERINVOICE_YEARID() As CrystalDecisions.[Shared].IParameterField
-        Get
-            Return Me.DataDefinition.ParameterFields(2)
-        End Get
-    End Property
 End Class
 
 <System.Drawing.ToolboxBitmapAttribute(GetType(CrystalDecisions.[Shared].ExportOptions), "report.bmp")>  _
