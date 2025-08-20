75ae7ced7322119518e0fa7c07e428e75a059b1c
NakodaInfotech PRAVEEN <sales@nakodainfotech.com>
2025-08-20 16:21:29 +0530

    Update AgencyOpeningBills.vb

--- TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Agency Module/Agency Opening/AgencyOpeningBills.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Agency Module/Agency Opening/AgencyOpeningBills.vb
@@ -277,7 +277,10 @@ Public Class AgencyOpeningBills
         Try
             'If cmbname.Text.Trim <> "" Then ledgervalidate(cmbname, CMBACCCODE, e, Me, txtadd, " and (groupmaster.group_SECONDARY = 'Sundry Debtors' or groupmaster.group_SECONDARY = 'Indirect Income' or groupmaster.group_SECONDARY = 'Direct Income') and acc_cmpid = " & CmpId & " and acc_LOCATIONid = " & Locationid & " and acc_YEARid = " & YearId)
             If CMBNAME.Text.Trim <> "" Then ledgervalidate(CMBNAME, CMBACCCODE, e, Me, TXTADD, " AND (GROUPMASTER.GROUP_SECONDARY ='SUNDRY DEBTORS' OR GROUPMASTER.GROUP_SECONDARY ='SUNDRY CREDITORS')and acc_cmpid = " & CmpId & " and acc_LOCATIONid = " & Locationid & " and acc_YEARid = " & YearId)
-            If TXTBILLNO.Text.Trim = "" And CMBNAME.Text.Trim <> "" And CMBDELIVERYAT.Text.Trim <> "" Then
+            'If TXTBILLNO.Text.Trim = "" And CMBNAME.Text.Trim <> "" And CMBDELIVERYAT.Text.Trim <> "" Then
+            '    FILLGRIDOPENING()
+            'End If
+            If TXTBILLNO.Text.Trim = "" And CMBNAME.Text.Trim <> "" Then
                 FILLGRIDOPENING()
             End If
         Catch ex As Exception
