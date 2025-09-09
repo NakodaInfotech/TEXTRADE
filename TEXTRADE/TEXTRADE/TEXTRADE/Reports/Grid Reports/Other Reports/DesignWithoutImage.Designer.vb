641092c0bda0682a3b61d8852360983e1d674a88
NakodaInfotech <sales@nakodainfotech.com>
2025-09-09 20:09:44 +0530

    designwithout

---
+++ TEXTRADE/TEXTRADE/TEXTRADE/Reports/Grid Reports/Other Reports/DesignWithoutImage.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DesignWithoutImage
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BlendPanel1 = New VbPowerPack.BlendPanel()
+        Me.CMDEXIT = New System.Windows.Forms.Button()
+        Me.GRIDBILLDETAILS = New DevExpress.XtraGrid.GridControl()
+        Me.GRIDBILL = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GSRNO = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GITEMNAME = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GDESIGN = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.toolStripSeparator = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExcelExport = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BlendPanel1.SuspendLayout()
+        CType(Me.GRIDBILLDETAILS, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GRIDBILL, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'BlendPanel1
+        '
+        Me.BlendPanel1.Blend = New VbPowerPack.BlendFill(VbPowerPack.BlendStyle.Vertical, System.Drawing.Color.FromArgb(CType(CType(213, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(248, Byte), Integer)), System.Drawing.SystemColors.Window)
+        Me.BlendPanel1.Controls.Add(Me.CMDEXIT)
+        Me.BlendPanel1.Controls.Add(Me.GRIDBILLDETAILS)
+        Me.BlendPanel1.Controls.Add(Me.ToolStrip1)
+        Me.BlendPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.BlendPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.BlendPanel1.Name = "BlendPanel1"
+        Me.BlendPanel1.Size = New System.Drawing.Size(1234, 581)
+        Me.BlendPanel1.TabIndex = 7
+        '
+        'CMDEXIT
+        '
+        Me.CMDEXIT.BackColor = System.Drawing.Color.Transparent
+        Me.CMDEXIT.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.CMDEXIT.FlatAppearance.BorderSize = 0
+        Me.CMDEXIT.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CMDEXIT.ForeColor = System.Drawing.Color.Black
+        Me.CMDEXIT.Location = New System.Drawing.Point(529, 541)
+        Me.CMDEXIT.Name = "CMDEXIT"
+        Me.CMDEXIT.Size = New System.Drawing.Size(80, 28)
+        Me.CMDEXIT.TabIndex = 322
+        Me.CMDEXIT.Text = "E&xit"
+        Me.CMDEXIT.UseVisualStyleBackColor = False
+        '
+        'GRIDBILLDETAILS
+        '
+        Me.GRIDBILLDETAILS.Location = New System.Drawing.Point(12, 45)
+        Me.GRIDBILLDETAILS.LookAndFeel.UseDefaultLookAndFeel = False
+        Me.GRIDBILLDETAILS.MainView = Me.GRIDBILL
+        Me.GRIDBILLDETAILS.Name = "GRIDBILLDETAILS"
+        Me.GRIDBILLDETAILS.Size = New System.Drawing.Size(951, 490)
+        Me.GRIDBILLDETAILS.TabIndex = 315
+        Me.GRIDBILLDETAILS.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GRIDBILL})
+        '
+        'GRIDBILL
+        '
+        Me.GRIDBILL.Appearance.Row.Font = New System.Drawing.Font("Calibri", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GRIDBILL.Appearance.Row.Options.UseFont = True
+        Me.GRIDBILL.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GSRNO, Me.GITEMNAME, Me.GDESIGN})
+        Me.GRIDBILL.GridControl = Me.GRIDBILLDETAILS
+        Me.GRIDBILL.Name = "GRIDBILL"
+        Me.GRIDBILL.OptionsBehavior.AutoExpandAllGroups = True
+        Me.GRIDBILL.OptionsBehavior.Editable = False
+        Me.GRIDBILL.OptionsCustomization.AllowGroup = False
+        Me.GRIDBILL.OptionsView.ColumnAutoWidth = False
+        Me.GRIDBILL.OptionsView.GroupFooterShowMode = DevExpress.XtraGrid.Views.Grid.GroupFooterShowMode.VisibleAlways
+        Me.GRIDBILL.OptionsView.ShowAutoFilterRow = True
+        Me.GRIDBILL.OptionsView.ShowGroupPanel = False
+        '
+        'GSRNO
+        '
+        Me.GSRNO.Caption = "Sr. No"
+        Me.GSRNO.FieldName = "ITEMDESIGNNO"
+        Me.GSRNO.Name = "GSRNO"
+        Me.GSRNO.Visible = True
+        Me.GSRNO.VisibleIndex = 0
+        Me.GSRNO.Width = 50
+        '
+        'GITEMNAME
+        '
+        Me.GITEMNAME.Caption = "Item Name"
+        Me.GITEMNAME.FieldName = "ITEMNAME"
+        Me.GITEMNAME.Name = "GITEMNAME"
+        Me.GITEMNAME.Visible = True
+        Me.GITEMNAME.VisibleIndex = 1
+        Me.GITEMNAME.Width = 250
+        '
+        'GDESIGN
+        '
+        Me.GDESIGN.Caption = "Design Name"
+        Me.GDESIGN.FieldName = "DESIGNNAME"
+        Me.GDESIGN.Name = "GDESIGN"
+        Me.GDESIGN.Visible = True
+        Me.GDESIGN.VisibleIndex = 2
+        Me.GDESIGN.Width = 250
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.toolStripSeparator, Me.ExcelExport, Me.ToolStripSeparator2})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(1234, 25)
+        Me.ToolStrip1.TabIndex = 318
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'toolStripSeparator
+        '
+        Me.toolStripSeparator.Name = "toolStripSeparator"
+        Me.toolStripSeparator.Size = New System.Drawing.Size(6, 25)
+        '
+        'ExcelExport
+        '
+        Me.ExcelExport.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ExcelExport.Image = Global.TEXTRADE.My.Resources.Resources.Excel_icon
+        Me.ExcelExport.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ExcelExport.Name = "ExcelExport"
+        Me.ExcelExport.Size = New System.Drawing.Size(23, 22)
+        Me.ExcelExport.Text = "&Export to Excel"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(6, 25)
+        '
+        'DesignWithoutImage
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(1234, 581)
+        Me.Controls.Add(Me.BlendPanel1)
+        Me.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.KeyPreview = True
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Name = "DesignWithoutImage"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "DesignWithoutImage"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.BlendPanel1.ResumeLayout(False)
+        Me.BlendPanel1.PerformLayout()
+        CType(Me.GRIDBILLDETAILS, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GRIDBILL, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BlendPanel1 As VbPowerPack.BlendPanel
+    Friend WithEvents CMDEXIT As Button
+    Friend WithEvents GRIDBILLDETAILS As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GRIDBILL As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GSRNO As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GITEMNAME As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GDESIGN As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents toolStripSeparator As ToolStripSeparator
+    Friend WithEvents ExcelExport As ToolStripButton
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+End Class
---
+++ TEXTRADE/TEXTRADE/TEXTRADE/Reports/Grid Reports/Other Reports/DesignWithoutImage.vb
@@ -0,0 +1,49 @@
+﻿Imports BL
+Imports DevExpress.XtraGrid.Views.Base
+Imports SystemWindows.Forms
+Public Class DesignWithoutImage
+    Dim USERADD, USEREDIT, USERVIEW, USERDELETE As Boolean
+    Private Sub CMDEXIT_Click(sender As Object, e As EventArgs) Handles CMDEXIT.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ExcelExport_Click(sender As Object, e As EventArgs) Handles ExcelExport.Click
+        Try
+            Dim PATH As String = "" = ""
+            If FileIO.FileSystem.FileExists(PATH) = True Then FileIO.FileSystem.DeleteFile(PATH)
+            PATH = Application.StartupPath & "\Design Without Image.XLS"
+
+            Dim opti As New DevExpress.XtraPrinting.XlsExportOptions
+            opti.ShowGridLines = True
+
+            Dim PERIOD As String = AccFrom & " - " & AccTo
+            opti.SheetName = "Design Without Image"
+            GRIDBILL.ExportToXls(PATH, opti)
+            EXCELCMPHEADER(PATH, "Design Without Image", GRIDBILL.VisibleColumns.Count + GRIDBILL.GroupCount, "", PERIOD)
+
+        Catch ex As Exception
+            MsgBox("Design Without Image Excel File is Open, Please Close the File first then try to Export", MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    Private Sub DesignWithoutImage_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+
+            Dim DTROW() As DataRow = USERRIGHTS.Select("FormName = 'DESIGN MASTER'")
+            USERADD = DTROW(0).Item(1)
+            USEREDIT = DTROW(0).Item(2)
+            USERVIEW = DTROW(0).Item(3)
+            USERDELETE = DTROW(0).Item(4)
+
+
+            If USEREDIT = False And USERVIEW = False Then
+                MsgBox("Insufficient Rights")
+                Exit Sub
+            End If
+
+            ' FILLGRID()
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+End Class
