fbc23a671e64d91513072ec5bb49d0e91db1941d
Gulkit <sales@nakodainfotech.com>
2025-09-09 18:17:09 +0530

    AARYA CHANGES

--- TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Purchase/Purchase Return/PurchaseReturnChallan.Designer.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Purchase/Purchase Return/PurchaseReturnChallan.Designer.vb
@@ -35,6 +35,7 @@ Partial Class PurchaseReturnChallan
         Dim DataGridViewCellStyle8 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(PurchaseReturnChallan))
         Me.BlendPanel1 = New VbPowerPack.BlendPanel()
+        Me.CMBDISPATCHTO = New System.Windows.Forms.ComboBox()
         Me.LBLDISPATCHTO = New System.Windows.Forms.Label()
         Me.CMBTRANSPORTNAME = New System.Windows.Forms.ComboBox()
         Me.PRLRDATE = New System.Windows.Forms.MaskedTextBox()
@@ -129,7 +130,6 @@ Partial Class PurchaseReturnChallan
         Me.toolnext = New System.Windows.Forms.ToolStripButton()
         Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
         Me.Ep = New System.Windows.Forms.ErrorProvider(Me.components)
-        Me.CMBDISPATCHTO = New System.Windows.Forms.ComboBox()
         Me.BlendPanel1.SuspendLayout()
         CType(Me.PBlock, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.TabControl1.SuspendLayout()
@@ -192,16 +192,29 @@ Partial Class PurchaseReturnChallan
         Me.BlendPanel1.Size = New System.Drawing.Size(1234, 581)
         Me.BlendPanel1.TabIndex = 0
         '
+        'CMBDISPATCHTO
+        '
+        Me.CMBDISPATCHTO.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.CMBDISPATCHTO.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.CMBDISPATCHTO.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CMBDISPATCHTO.FormattingEnabled = True
+        Me.CMBDISPATCHTO.Items.AddRange(New Object() {""})
+        Me.CMBDISPATCHTO.Location = New System.Drawing.Point(750, 129)
+        Me.CMBDISPATCHTO.Name = "CMBDISPATCHTO"
+        Me.CMBDISPATCHTO.Size = New System.Drawing.Size(203, 23)
+        Me.CMBDISPATCHTO.TabIndex = 909
+        Me.CMBDISPATCHTO.Visible = False
+        '
         'LBLDISPATCHTO
         '
         Me.LBLDISPATCHTO.AutoSize = True
         Me.LBLDISPATCHTO.BackColor = System.Drawing.Color.Transparent
         Me.LBLDISPATCHTO.Font = New System.Drawing.Font("Calibri", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LBLDISPATCHTO.Location = New System.Drawing.Point(677, 132)
+        Me.LBLDISPATCHTO.Location = New System.Drawing.Point(698, 133)
         Me.LBLDISPATCHTO.Name = "LBLDISPATCHTO"
-        Me.LBLDISPATCHTO.Size = New System.Drawing.Size(70, 14)
+        Me.LBLDISPATCHTO.Size = New System.Drawing.Size(46, 14)
         Me.LBLDISPATCHTO.TabIndex = 1019
-        Me.LBLDISPATCHTO.Text = "Dispatch To"
+        Me.LBLDISPATCHTO.Text = "Ship To"
         Me.LBLDISPATCHTO.Visible = False
         '
         'CMBTRANSPORTNAME
@@ -1257,19 +1270,6 @@ Partial Class PurchaseReturnChallan
         Me.Ep.BlinkRate = 0
         Me.Ep.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink
         Me.Ep.ContainerControl = Me
-        '
-        'CMBDISPATCHTO
-        '
-        Me.CMBDISPATCHTO.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
-        Me.CMBDISPATCHTO.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
-        Me.CMBDISPATCHTO.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CMBDISPATCHTO.FormattingEnabled = True
-        Me.CMBDISPATCHTO.Items.AddRange(New Object() {""})
-        Me.CMBDISPATCHTO.Location = New System.Drawing.Point(750, 128)
-        Me.CMBDISPATCHTO.Name = "CMBDISPATCHTO"
-        Me.CMBDISPATCHTO.Size = New System.Drawing.Size(203, 23)
-        Me.CMBDISPATCHTO.TabIndex = 909
-        Me.CMBDISPATCHTO.Visible = False
         '
         'PurchaseReturnChallan
         '
