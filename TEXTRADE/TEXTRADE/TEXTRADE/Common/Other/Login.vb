29c14fe677ae9fa1db92ac60f4cab13c3a6b4ba6
Moin <sales@nakodainfotech.com>
2025-11-03 19:28:25 +0530

    SUPEEMA PAYROLL AND JOBIN

--- TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
@@ -932,6 +932,7 @@ line1:
                     HIDECATALOG = False
                     HIDEDYEINGPROGRAM = False
                     HIDESTORES = False
+                    HIDEPAYROLL = False
                     HIGHVERSION = True
                     If Now.Date > DateTime.Parse("15.04.2026 00:00") Then
                         Dim DTNEW As DataTable = OBJCMN.Execute_Any_String("UPDATE VERSION SET VERSION_NO='1.0.0000'", "", "")
