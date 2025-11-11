b68488ec6fbc16d1807f5f4c04a93892c8879a49
NakodaInfotech <sales@nakodainfotech.com>
2025-11-11 10:37:35 +0530

    Paramount Login

--- TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
@@ -667,7 +667,7 @@ line1:
                     End If
                 ElseIf ClientName = "PARAMOUNT" Then
                     HIDEACCOUNTS = True
-                    If Now.Date > DateTime.Parse("15.11.2025 00:00") Then
+                    If Now.Date > DateTime.Parse("15.11.2026 00:00") Then
                         Dim DTNEW As DataTable = OBJCMN.Execute_Any_String("UPDATE VERSION SET VERSION_NO='1.0.0000'", "", "")
                         GoTo LINE1
                     End If
