6e28d781428d868df7f5e355c326c082adb2540f
NakodaInfotech <sales@nakodainfotech.com>
2025-11-19 16:08:44 +0530

    CHANGES

--- TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Sale/Sale Order/SaleOrder.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Sale/Sale Order/SaleOrder.vb
@@ -2805,7 +2805,7 @@ LINESINGLE:
                     If DTRATE.Rows.Count > 0 AndAlso Val(TXTRATE.Text.Trim) = 0 Then TXTRATE.Text = Val(DTRATE.Rows(0).Item("RATE"))
                 End If
 
-                If (ClientName = "MAHAVIR" Or ClientName = "BARKHA" Or ClientName = "MAHAJAN" Or ClientName = "SHUBHI" Or ClientName = "SUBHLAXMI" Or ClientName = "SMS" Or ClientName = "RAJKRIPA" Or ClientName = "MAHAVIRPOLYCOT" Or ClientName = "SIDDHPOLYCOT" Or ClientName = "SIDDHGIRI" Or ClientName = "MASHOK" Or ClientName = "AFW" Or ClientName = "SHEETAL") Then
+                If (ClientName = "MAHAVIR" Or ClientName = "BARKHA" Or ClientName = "MAHAJAN" Or ClientName = "SHUBHI" Or ClientName = "SUBHLAXMI" Or ClientName = "SMS" Or ClientName = "RAJKRIPA" Or ClientName = "MAHAVIRPOLYCOT" Or ClientName = "SIDDHPOLYCOT" Or ClientName = "SIDDHGIRI" Or ClientName = "MASHOK" Or ClientName = "AFW" Or ClientName = "SHEETAL" Or ClientName = "MILUXE") Then
                     DT = OBJCMN.SEARCH("  ISNULL(item_reorder, 0) AS CUT, ISNULL(ITEM_RATE, 0) AS RATE,ISNULL(ITEM_FOLD, '') AS [DESC],ISNULL(UNITMASTER.unit_abbr, '') AS UNIT, ISNULL(CATEGORY_NAME,'') AS CATEGORY", "", " ITEMMASTER LEFT OUTER JOIN CATEGORYMASTER ON ITEM_CATEGORYID = CATEGORY_ID LEFT OUTER JOIN UNITMASTER ON ITEMMASTER.item_unitid = UNITMASTER.unit_id ", " AND ITEMMASTER.item_name = '" & cmbitemname.Text.Trim & "' AND ITEMMASTER.ITEM_YEARID='" & YearId & "' ")
                     If DT.Rows.Count > 0 Then
                         If ClientName <> "SIDDHGIRI" And ClientName <> "MASHOK" And ClientName <> "ABHEE" And ClientName <> "AFW" Then TXTCUT.Text = DT.Rows(0).Item("CUT")
