df35e346c47a08f84b29a55252a6254320a35d39
Waseem Shaikh <sales@nakodainfotech.com>
2025-12-17 18:10:17 +0530

    ABHEE WORK

--- TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Accounts/Credit Note/CreditNote.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Accounts/Credit Note/CreditNote.vb
@@ -25,6 +25,8 @@ Public Class CREDITNOTE
     Dim TEMPCHGSROW, TEMPADJROW As Integer
     Dim a As Integer = 0
     Dim col As New DataGridViewCheckBoxColumn
+    Private DT As DataTable
+
 
     Private Sub cmdexit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdexit.Click
         Try
@@ -1711,6 +1713,8 @@ LINE1:
             Dim DT As New DataTable
             DT = objpayment.GETBILLS(CmpId, CMBNAME.Text.Trim, Locationid, YearId, Convert.ToDateTime(CNDATE.Text).Date)
             If DT.Rows.Count > 0 Then SETGRIDINVOICE(DT)
+            CreateFilterTextBoxes()
+
         Catch ex As Exception
             Throw ex
         End Try
@@ -3840,4 +3844,92 @@ LINE1:                      'GET INVPRINTTINITIALS | PCS | MTRS | BILLAMT
             Throw ex
         End Try
     End Sub
+#Region "AUTOSEARCHTEXTBOX"
+
+    Public filterTextBoxes As New List(Of TextBox)
+
+    ' Call this after setting new data (e.g., on "Display" click)
+    Public Sub CreateFilterTextBoxes()
+
+        'REMOVE OLD TEXTBOXES AND THEN RECREATE
+        For i As Integer = groupbill.Controls.Count - 1 To 0 Step -1
+            If TypeOf groupbill.Controls(i) Is TextBox Then
+                groupbill.Controls.RemoveAt(i)
+            End If
+        Next
+        filterTextBoxes.Clear()
+        If GRIDBILL.Columns.Count = 0 Then Exit Sub
+        Dim xPos As Integer = GRIDBILL.RowHeadersVisible * GRIDBILL.RowHeadersWidth
+        For Each col As DataGridViewColumn In GRIDBILL.Columns
+            If col.Visible Then
+                Dim txt As New TextBox()
+                txt.Width = col.Width
+                txt.Left = GRIDBILL.GetCellDisplayRectangle(col.Index, -1, True).Left
+                txt.Top = 5 ' Or a header-compliant Y offset
+                txt.Tag = col.Index
+                txt.Name = "TXT" & col.Index
+                AddHandler txt.TextChanged, AddressOf FilterGrid
+                groupbill.Controls.Add(txt)
+                filterTextBoxes.Add(txt)
+            End If
+        Next
+    End Sub
+
+    Public Sub FilterGrid(sender As Object, e As EventArgs)
+        'Try
+        '    Dim filterClauses As New List(Of String)()
+        '    For Each txt As TextBox In filterTextBoxes
+        '        Dim colIndex As Integer = CInt(txt.Tag)
+        '        Dim colName As String = GRIDBILL.Columns(colIndex).DataPropertyName
+        '        Dim filterText As String = txt.Text.Trim().Replace("'", "''")
+
+        '        If filterText <> "" Then
+        '            ' Check data type
+        '            Dim colType As Type = DT.Columns(colName).DataType
+        '            If colType Is GetType(String) Then
+        '                filterClauses.Add(String.Format("[{0}] LIKE '%{1}%'", colName, filterText))
+        '            ElseIf colType Is GetType(Double) OrElse colType Is GetType(Integer) Then
+        '                ' Numeric filter: try direct match
+        '                Dim valDouble As Double
+        '                If Double.TryParse(filterText, valDouble) Then
+        '                    filterClauses.Add(String.Format("[{0}] = {1}", colName, valDouble))
+        '                End If
+        '            ElseIf colType Is GetType(DateTime) Then
+        '                Dim valDate As DateTime
+        '                If DateTime.TryParse(filterText, valDate) Then
+        '                    filterClauses.Add(String.Format("[{0}] = #{1}#", colName, valDate.ToString("MM/dd/yyyy")))
+        '                End If
+        '            ElseIf colType Is GetType(Date) OrElse colType Is GetType(DateTime) Then
+        '                Dim valDate As DateTime
+        '                If DateTime.TryParse(filterText, valDate) Then
+        '                    ' For exact date match
+        '                    filterClauses.Add(String.Format("[{0}] = #{1}#", colName, valDate.ToString("MM/dd/yyyy")))
+
+        '                    ' Or you can do range filtering, example hardcoded here (customize as needed)
+        '                    ' filterClauses.Add(String.Format("[{0}] >= #{1}# AND [{0}] <= #{2}#", colName, valDate.AddDays(-1).ToString("MM/dd/yyyy"), valDate.AddDays(1).ToString("MM/dd/yyyy")))
+        '                End If
+
+        '            End If
+        '        End If
+        '    Next
+
+
+        '    Dim filterString As String = String.Join(" AND ", filterClauses)
+        '    DT.DefaultView.RowFilter = filterString
+        'Catch ex As Exception
+        '    MsgBox("Error while filtering: " & ex.Message)
+        'End Try
+    End Sub
+    Private Sub gridbill_SortCompare(sender As Object, e As DataGridViewSortCompareEventArgs) Handles GRIDBILL.SortCompare
+        Try
+            If GRIDBILL.ColumnCount = 15 And e.Column.Index > 1 Then
+                e.SortResult = CDbl(e.CellValue1).CompareTo(CDbl(e.CellValue2))
+                e.Handled = True
+            End If
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+#End Region
 End Class
