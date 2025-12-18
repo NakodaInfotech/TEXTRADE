1cc72db03530d2cec83612c87f8816f0c745b9ec
Moin <sales@nakodainfotech.com>
2025-12-18 14:55:47 +0530

    NEW CLIENT LOGIN

--- TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
@@ -769,6 +769,11 @@ line1:
                         Dim DTNEW As DataTable = OBJCMN.Execute_Any_String("UPDATE VERSION SET VERSION_NO='1.0.0000'", "", "")
                         GoTo LINE1
                     End If
+                ElseIf ClientName = "ROSIMI" Then
+                    If Now.Date > DateTime.Parse("15.01.2026 00:00") Then
+                        Dim DTNEW As DataTable = OBJCMN.Execute_Any_String("UPDATE VERSION SET VERSION_NO='1.0.0000'", "", "")
+                        GoTo LINE1
+                    End If
                 ElseIf ClientName = "RUCHITA" Then
                     HIDEACCOUNTS = True
                     HIDESAMPLEMODULE = False
