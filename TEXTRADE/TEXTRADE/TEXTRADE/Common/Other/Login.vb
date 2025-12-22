716feb6133c97150fc02d66faf1b32eb569cabb8
Moin <sales@nakodainfotech.com>
2025-12-22 13:54:06 +0530

    changes kencot

--- TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
@@ -431,7 +431,6 @@ line1:
                         GoTo LINE1
                     End If
                 ElseIf ClientName = "KENCOT" Then
-                    HIDEACCOUNTSEXCEPTINVOICE = True
                     If Now.Date > DateTime.Parse("15.04.2026 00:00") Then
                         Dim DTNEW As DataTable = OBJCMN.Execute_Any_String("UPDATE VERSION SET VERSION_NO='1.0.0000'", "", "")
                         GoTo LINE1
