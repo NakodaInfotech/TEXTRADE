750579a237cbed4e4db6db77f80f33ffb07f1b92
NakodaInfotech <sales@nakodainfotech.com>
2026-01-01 18:18:14 +0530

    Temp Sold

--- TEXTRADE/TEXTRADE/BL/Transactions/Purchase/ClsPurchaseMaster.vb
+++ TEXTRADE/TEXTRADE/BL/Transactions/Purchase/ClsPurchaseMaster.vb
@@ -333,6 +333,8 @@ Public Class ClsPurchaseMaster
                 I = I + 1
                 .Add(New SqlClient.SqlParameter("@COMPLAINTDATE", alParaval(I)))
                 I = I + 1
+                .Add(New SqlClient.SqlParameter("@TEMPSOLD", alParaval(I)))
+                I = I + 1
             End With
 
             DTTABLE = objDBOperation.execute(strCommand, alParameter).Tables(0)
@@ -660,6 +662,8 @@ Public Class ClsPurchaseMaster
                 .Add(New SqlClient.SqlParameter("@COMPLAINTBY", alParaval(I)))
                 I = I + 1
                 .Add(New SqlClient.SqlParameter("@COMPLAINTDATE", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@TEMPSOLD", alParaval(I)))
                 I = I + 1
 
                 .Add(New SqlClient.SqlParameter("@BILLNO", alParaval(I)))
