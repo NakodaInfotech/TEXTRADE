be24aefbe7af8d2c8b03af37fca910a9af2f53b8
Moin <sales@nakodainfotech.com>
2026-02-10 11:33:35 +0530

    chnages

---
+++ TEXTRADE/TEXTRADE/BL/Transactions/Yarn Modules/Yarn Inter GodownTransfer/ClsYarnInterGodownTransfer.vb
@@ -0,0 +1,248 @@
+﻿Imports DB
+Public Class ClsYarnInterGodownTransfer
+    Private objDBOperation As DBOperation
+    Public alParaval As New ArrayList
+
+#Region "Constructor"
+    Public Sub New()
+        Try
+            objDBOperation = New DBOperation
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+#End Region
+
+#Region "Functions"
+
+    Public Function SAVE() As DataTable
+        Dim DTTABLE As DataTable
+        Try
+            'save SALE order
+            Dim strCommand As String = "SP_TRANS_YARNINTERGODOWNTRANSFER_SAVE"
+            Dim alParameter As New ArrayList
+            With alParameter
+
+                Dim I As Integer = 0
+                .Add(New SqlClient.SqlParameter("@GODOWNNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@DATE", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@FROMGODOWN", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@TOGODOWN", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@TRANSPORTNAME", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@REFRENCE", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@ISSUE", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@LBLTOTALPCS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@LBLTOTALMTRS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@REMARKS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@cmpid", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@locationid", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@userid", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@yearid", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@transfer", alParaval(I)))
+                I = I + 1
+
+                'grid parameters
+                .Add(New SqlClient.SqlParameter("@gridsrno", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@YARNQUALITY", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@MILLNAME", alParaval(I)))
+                I = I + 1
+
+                .Add(New SqlClient.SqlParameter("@DESIGN", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@PARTYLOTNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@PARTYCOLOR", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@COLOR", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@LOTNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@BAGS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@WEIGHT", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@CONES", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@LRNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@LRDATE", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@LIFTINGDATE", alParaval(I)))
+                I = I + 1
+
+                .Add(New SqlClient.SqlParameter("@BARCODE", alParaval(I))) 'BARCODE ADDED
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@OUTPCS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@OUTMTRS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@PONO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@GRIDGSRNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@FROMTYPE", alParaval(I)))
+                I = I + 1
+            End With
+
+            DTTABLE = objDBOperation.execute(strCommand, alParameter).Tables(0)
+
+        Catch ex As Exception
+            Throw ex
+        End Try
+        Return DTTABLE
+    End Function
+
+    Public Function UPDATE() As Integer
+        Dim intResult As Integer
+        Try
+            'Update SALE order
+            Dim strCommand As String = "SP_TRANS_YARNINTERGODOWNTRANSFER_UPDATE"
+            Dim alParameter As New ArrayList
+            With alParameter
+
+
+                Dim I As Integer = 0
+                .Add(New SqlClient.SqlParameter("@GODOWNNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@DATE", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@FROMGODOWN", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@TOGODOWN", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@TRANSPORTNAME", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@REFRENCE", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@ISSUE", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@LBLTOTALPCS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@LBLTOTALMTRS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@REMARKS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@cmpid", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@locationid", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@userid", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@yearid", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@transfer", alParaval(I)))
+                I = I + 1
+                'grid parameters
+                .Add(New SqlClient.SqlParameter("@gridsrno", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@YARNQUALITY", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@MILLNAME", alParaval(I)))
+                I = I + 1
+
+                .Add(New SqlClient.SqlParameter("@DESIGN", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@PARTYLOTNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@PARTYCOLOR", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@COLOR", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@LOTNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@BAGS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@WEIGHT", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@CONES", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@LRNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@LRDATE", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@LIFTINGDATE", alParaval(I)))
+                I = I + 1
+
+                .Add(New SqlClient.SqlParameter("@BARCODE", alParaval(I))) 'BARCODE ADDED
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@OUTPCS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@OUTMTRS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@PONO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@GRIDGSRNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@FROMTYPE", alParaval(I)))
+                I = I + 1
+
+                .Add(New SqlClient.SqlParameter("@TEMPGODOWNNO", alParaval(I)))
+                I = I + 1
+
+            End With
+            intResult = objDBOperation.executeNonQuery(strCommand, alParameter)
+
+        Catch ex As Exception
+            Throw ex
+        End Try
+        Return 0
+    End Function
+
+    Public Function SELECTGODOWN() As DataTable
+        Dim dtTable As DataTable
+        Try
+
+            Dim strCommand As String = "SP_SELECTGODOWN_FOR_EDIT"
+            Dim alParameter As New ArrayList
+            With alParameter
+                .Add(New SqlClient.SqlParameter("@TEMPGODOWNNO", alParaval(0)))
+                .Add(New SqlClient.SqlParameter("@CmpID", alParaval(1)))
+                .Add(New SqlClient.SqlParameter("@LocationID", alParaval(2)))
+                .Add(New SqlClient.SqlParameter("@YearID", alParaval(3)))
+            End With
+            dtTable = objDBOperation.execute(strCommand, alParameter).Tables(0)
+
+        Catch ex As Exception
+            Throw ex
+        End Try
+        Return dtTable
+    End Function
+
+    Public Function Delete() As Integer
+        Dim intResult As Integer
+        Try
+            Dim strCommand As String = "SP_TRANS_YARNINTERGODOWNTRANSFER_DELETE"
+            Dim alParameter As New ArrayList
+            With alParameter
+                .Add(New SqlClient.SqlParameter("@TEMPGODOWNNO", alParaval(0)))
+                .Add(New SqlClient.SqlParameter("@Cmpid", alParaval(1)))
+                .Add(New SqlClient.SqlParameter("@LocationID", alParaval(2)))
+                .Add(New SqlClient.SqlParameter("@YearID", alParaval(3)))
+            End With
+            intResult = objDBOperation.executeNonQuery(strCommand, alParameter)
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+
+
+
+#End Region
+
+End Class
