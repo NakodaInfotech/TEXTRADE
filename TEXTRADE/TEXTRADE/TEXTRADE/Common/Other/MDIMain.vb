d9e11b3f7eec293918395763a1aa532e2bb77965
Chandrish Pal <sales@nakodainfotech.com>
2026-02-10 15:59:26 +0530

    AVC

--- TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/MDIMain.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/MDIMain.vb
@@ -6516,13 +6516,7 @@ SKIPLINE:
                         ALPARAVAL.Add("")   'WARPAMOUNT
 
 
-                        ALPARAVAL.Add("")   'WEFTSRNO
-                        ALPARAVAL.Add("")   'WEFTQUALITY
-                        ALPARAVAL.Add("")   'WEFTSHADE
-                        ALPARAVAL.Add("")   'WEFTPICK
-                        ALPARAVAL.Add("")   'WEFTWT
-                        ALPARAVAL.Add("")   'WEFTRATE
-                        ALPARAVAL.Add("")   'WEFTAMOUNT
+
 
                         ALPARAVAL.Add(0)    'WARPTL
                         ALPARAVAL.Add(0)    'WEFTTL
@@ -6558,6 +6552,8 @@ SKIPLINE:
                         ALPARAVAL.Add(0)    'ITEM GSM
                         ALPARAVAL.Add(0)    'ITEM PERCENT
                         ALPARAVAL.Add(0)    'GARMENT
+                        ALPARAVAL.Add(0)    'TOTALBEAMENDS
+                        ALPARAVAL.Add(0)    'TOTALWEFTENDS
 
                         ALPARAVAL.Add(0)    'SHADESRNO
                         ALPARAVAL.Add(0)    'SHADECOLORID
@@ -6568,6 +6564,16 @@ SKIPLINE:
                         ALPARAVAL.Add(0)    'SHADEITEMCOLORID
                         ALPARAVAL.Add(0)    'SHADEMTRS
                         ALPARAVAL.Add(0)    'SHADEsrno
+
+                        ALPARAVAL.Add("")   'WEFTSRNO
+                        ALPARAVAL.Add("")   'WEFTQUALITY
+                        ALPARAVAL.Add("")   'WEFTSHADE
+                        ALPARAVAL.Add("")   'WEFTPICK
+                        ALPARAVAL.Add("")   'WEFTWT
+                        ALPARAVAL.Add("")   'WEFTRATE
+                        ALPARAVAL.Add("")   'WEFTAMOUNT
+                        ALPARAVAL.Add(0)    'WEFTSHADESRNO
+
 
                         Dim objclsItemMaster As New clsItemmaster
                         objclsItemMaster.alParaval = ALPARAVAL
