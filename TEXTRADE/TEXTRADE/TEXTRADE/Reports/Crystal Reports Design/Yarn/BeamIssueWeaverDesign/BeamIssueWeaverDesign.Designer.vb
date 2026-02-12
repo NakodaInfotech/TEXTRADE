700cae80d335f68ded30d153f0977ab9a1ab3ff8
Moin <sales@nakodainfotech.com>
2026-02-12 17:29:33 +0530

    changes

---
+++ TEXTRADE/TEXTRADE/TEXTRADE/Reports/Crystal Reports Design/Yarn/BeamIssueWeaverDesign/BeamIssueWeaverDesign.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class BeamIssueWeaverDesign
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.SendMail = New System.Windows.Forms.ToolStripButton()
+        Me.TOOLWHATSAPP = New System.Windows.Forms.ToolStripButton()
+        Me.CRPO = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SendMail, Me.TOOLWHATSAPP})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(284, 25)
+        Me.ToolStrip1.TabIndex = 5
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'SendMail
+        '
+        Me.SendMail.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.SendMail.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.SendMail.Name = "SendMail"
+        Me.SendMail.Size = New System.Drawing.Size(23, 22)
+        Me.SendMail.Text = "ToolStripButton1"
+        '
+        'TOOLWHATSAPP
+        '
+        Me.TOOLWHATSAPP.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.TOOLWHATSAPP.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.TOOLWHATSAPP.Name = "TOOLWHATSAPP"
+        Me.TOOLWHATSAPP.Size = New System.Drawing.Size(23, 22)
+        Me.TOOLWHATSAPP.Text = "&Whatsapp"
+        '
+        'CRPO
+        '
+        Me.CRPO.ActiveViewIndex = -1
+        Me.CRPO.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.CRPO.Cursor = System.Windows.Forms.Cursors.Default
+        Me.CRPO.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.CRPO.EnableDrillDown = False
+        Me.CRPO.Location = New System.Drawing.Point(0, 25)
+        Me.CRPO.Name = "CRPO"
+        Me.CRPO.SelectionFormula = ""
+        Me.CRPO.Size = New System.Drawing.Size(284, 237)
+        Me.CRPO.TabIndex = 6
+        Me.CRPO.ViewTimeSelectionFormula = ""
+        '
+        'BeamIssueWeaverDesign
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.CRPO)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.KeyPreview = True
+        Me.Name = "BeamIssueWeaverDesign"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "BeamIssueWeaverDesign"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents SendMail As ToolStripButton
+    Friend WithEvents TOOLWHATSAPP As ToolStripButton
+    Friend WithEvents CRPO As CrystalDecisions.Windows.Forms.CrystalReportViewer
+End Class
