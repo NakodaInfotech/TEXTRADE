dfccb30694fa31afa09bb0e389759214c34c1210
Moin <sales@nakodainfotech.com>
2026-02-14 11:05:11 +0530

    changes

---
+++ TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Yarn Module/Yarn InterGodown Transfer/SelectYarnGodownTransfer.Designer.vb
@@ -0,0 +1,287 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SelectYarnGodownTransfer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BlendPanel1 = New VbPowerPack.BlendPanel()
+        Me.gridbilldetails = New DevExpress.XtraGrid.GridControl()
+        Me.gridbill = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GCHK = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.CHKEDIT = New DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit()
+        Me.GENTRYNO = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GSRNO = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GTYPE = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GMILLNAME = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GQUALITY = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GENDS = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GROLLS = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GWT = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GPROGRAMNO = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GTOTALENDS = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GLENGTH = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.APPROXDATE = New DevExpress.XtraEditors.Repository.RepositoryItemDateEdit()
+        Me.RepositoryItemCheckEdit1 = New DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit()
+        Me.cmdexit = New System.Windows.Forms.Button()
+        Me.cmdok = New System.Windows.Forms.Button()
+        Me.BlendPanel1.SuspendLayout()
+        CType(Me.gridbilldetails, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gridbill, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CHKEDIT, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.APPROXDATE, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.APPROXDATE.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RepositoryItemCheckEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BlendPanel1
+        '
+        Me.BlendPanel1.Blend = New VbPowerPack.BlendFill(VbPowerPack.BlendStyle.Vertical, System.Drawing.Color.FromArgb(CType(CType(213, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(248, Byte), Integer)), System.Drawing.SystemColors.Window)
+        Me.BlendPanel1.Controls.Add(Me.gridbilldetails)
+        Me.BlendPanel1.Controls.Add(Me.cmdexit)
+        Me.BlendPanel1.Controls.Add(Me.cmdok)
+        Me.BlendPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.BlendPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.BlendPanel1.Name = "BlendPanel1"
+        Me.BlendPanel1.Size = New System.Drawing.Size(1234, 581)
+        Me.BlendPanel1.TabIndex = 7
+        '
+        'gridbilldetails
+        '
+        Me.gridbilldetails.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gridbilldetails.Location = New System.Drawing.Point(21, 21)
+        Me.gridbilldetails.LookAndFeel.UseDefaultLookAndFeel = False
+        Me.gridbilldetails.MainView = Me.gridbill
+        Me.gridbilldetails.Name = "gridbilldetails"
+        Me.gridbilldetails.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.CHKEDIT, Me.APPROXDATE, Me.RepositoryItemCheckEdit1})
+        Me.gridbilldetails.Size = New System.Drawing.Size(986, 597)
+        Me.gridbilldetails.TabIndex = 648
+        Me.gridbilldetails.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gridbill})
+        '
+        'gridbill
+        '
+        Me.gridbill.Appearance.HeaderPanel.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gridbill.Appearance.HeaderPanel.Options.UseFont = True
+        Me.gridbill.Appearance.Row.Font = New System.Drawing.Font("Calibri", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gridbill.Appearance.Row.Options.UseFont = True
+        Me.gridbill.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GCHK, Me.GENTRYNO, Me.GSRNO, Me.GTYPE, Me.GMILLNAME, Me.GQUALITY, Me.GENDS, Me.GROLLS, Me.GWT, Me.GPROGRAMNO, Me.GTOTALENDS, Me.GLENGTH})
+        Me.gridbill.GridControl = Me.gridbilldetails
+        Me.gridbill.Name = "gridbill"
+        Me.gridbill.OptionsBehavior.AllowIncrementalSearch = True
+        Me.gridbill.OptionsView.ColumnAutoWidth = False
+        Me.gridbill.OptionsView.ShowAutoFilterRow = True
+        Me.gridbill.OptionsView.ShowFooter = True
+        Me.gridbill.OptionsView.ShowGroupPanel = False
+        '
+        'GCHK
+        '
+        Me.GCHK.ColumnEdit = Me.CHKEDIT
+        Me.GCHK.FieldName = "CHK"
+        Me.GCHK.Name = "GCHK"
+        Me.GCHK.OptionsColumn.ShowCaption = False
+        Me.GCHK.Visible = True
+        Me.GCHK.VisibleIndex = 0
+        Me.GCHK.Width = 57
+        '
+        'CHKEDIT
+        '
+        Me.CHKEDIT.AutoHeight = False
+        Me.CHKEDIT.Caption = ""
+        Me.CHKEDIT.Name = "CHKEDIT"
+        Me.CHKEDIT.NullStyle = DevExpress.XtraEditors.Controls.StyleIndeterminate.Unchecked
+        '
+        'GENTRYNO
+        '
+        Me.GENTRYNO.Caption = "Entry No"
+        Me.GENTRYNO.FieldName = "NO"
+        Me.GENTRYNO.Name = "GENTRYNO"
+        Me.GENTRYNO.OptionsColumn.AllowEdit = False
+        Me.GENTRYNO.Visible = True
+        Me.GENTRYNO.VisibleIndex = 1
+        '
+        'GSRNO
+        '
+        Me.GSRNO.Caption = "SrNo"
+        Me.GSRNO.FieldName = "SRNO"
+        Me.GSRNO.Name = "GSRNO"
+        Me.GSRNO.OptionsColumn.AllowEdit = False
+        '
+        'GTYPE
+        '
+        Me.GTYPE.Caption = "Type"
+        Me.GTYPE.FieldName = "TYPE"
+        Me.GTYPE.Name = "GTYPE"
+        Me.GTYPE.OptionsColumn.AllowEdit = False
+        Me.GTYPE.Visible = True
+        Me.GTYPE.VisibleIndex = 2
+        '
+        'GMILLNAME
+        '
+        Me.GMILLNAME.Caption = "Mill Name"
+        Me.GMILLNAME.FieldName = "MILLNAME"
+        Me.GMILLNAME.Name = "GMILLNAME"
+        Me.GMILLNAME.OptionsColumn.AllowEdit = False
+        Me.GMILLNAME.Visible = True
+        Me.GMILLNAME.VisibleIndex = 3
+        Me.GMILLNAME.Width = 300
+        '
+        'GQUALITY
+        '
+        Me.GQUALITY.Caption = "Quality"
+        Me.GQUALITY.FieldName = "YARNQUALITY"
+        Me.GQUALITY.Name = "GQUALITY"
+        Me.GQUALITY.OptionsColumn.AllowEdit = False
+        Me.GQUALITY.Visible = True
+        Me.GQUALITY.VisibleIndex = 4
+        Me.GQUALITY.Width = 200
+        '
+        'GENDS
+        '
+        Me.GENDS.Caption = "Ends"
+        Me.GENDS.DisplayFormat.FormatString = "0"
+        Me.GENDS.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric
+        Me.GENDS.FieldName = "ENDS"
+        Me.GENDS.Name = "GENDS"
+        Me.GENDS.OptionsColumn.AllowEdit = False
+        Me.GENDS.Visible = True
+        Me.GENDS.VisibleIndex = 5
+        '
+        'GROLLS
+        '
+        Me.GROLLS.Caption = "Rolls"
+        Me.GROLLS.FieldName = "ROLLS"
+        Me.GROLLS.Name = "GROLLS"
+        Me.GROLLS.OptionsColumn.AllowEdit = False
+        Me.GROLLS.Visible = True
+        Me.GROLLS.VisibleIndex = 6
+        '
+        'GWT
+        '
+        Me.GWT.Caption = "Weight"
+        Me.GWT.DisplayFormat.FormatString = "0.000"
+        Me.GWT.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric
+        Me.GWT.FieldName = "WT"
+        Me.GWT.Name = "GWT"
+        Me.GWT.OptionsColumn.AllowEdit = False
+        Me.GWT.Summary.AddRange(New DevExpress.XtraGrid.GridSummaryItem() {New DevExpress.XtraGrid.GridColumnSummaryItem(DevExpress.Data.SummaryItemType.Sum, "PCS", "")})
+        Me.GWT.Visible = True
+        Me.GWT.VisibleIndex = 7
+        '
+        'GPROGRAMNO
+        '
+        Me.GPROGRAMNO.Caption = "Program No"
+        Me.GPROGRAMNO.FieldName = "PROGRAMNO"
+        Me.GPROGRAMNO.Name = "GPROGRAMNO"
+        '
+        'GTOTALENDS
+        '
+        Me.GTOTALENDS.Caption = "Total Ends"
+        Me.GTOTALENDS.FieldName = "TOTALENDS"
+        Me.GTOTALENDS.Name = "GTOTALENDS"
+        '
+        'GLENGTH
+        '
+        Me.GLENGTH.Caption = "Length"
+        Me.GLENGTH.FieldName = "LENGTH"
+        Me.GLENGTH.Name = "GLENGTH"
+        '
+        'APPROXDATE
+        '
+        Me.APPROXDATE.AutoHeight = False
+        Me.APPROXDATE.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.APPROXDATE.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton()})
+        Me.APPROXDATE.Name = "APPROXDATE"
+        '
+        'RepositoryItemCheckEdit1
+        '
+        Me.RepositoryItemCheckEdit1.AutoHeight = False
+        Me.RepositoryItemCheckEdit1.Name = "RepositoryItemCheckEdit1"
+        '
+        'cmdexit
+        '
+        Me.cmdexit.BackColor = System.Drawing.Color.Transparent
+        Me.cmdexit.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.cmdexit.FlatAppearance.BorderSize = 0
+        Me.cmdexit.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdexit.ForeColor = System.Drawing.Color.Black
+        Me.cmdexit.Location = New System.Drawing.Point(517, 624)
+        Me.cmdexit.Name = "cmdexit"
+        Me.cmdexit.Size = New System.Drawing.Size(80, 28)
+        Me.cmdexit.TabIndex = 9
+        Me.cmdexit.Text = "E&xit"
+        Me.cmdexit.UseVisualStyleBackColor = False
+        '
+        'cmdok
+        '
+        Me.cmdok.BackColor = System.Drawing.Color.Transparent
+        Me.cmdok.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.cmdok.FlatAppearance.BorderSize = 0
+        Me.cmdok.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdok.ForeColor = System.Drawing.Color.Black
+        Me.cmdok.Location = New System.Drawing.Point(431, 624)
+        Me.cmdok.Name = "cmdok"
+        Me.cmdok.Size = New System.Drawing.Size(80, 28)
+        Me.cmdok.TabIndex = 8
+        Me.cmdok.Text = "&Ok"
+        Me.cmdok.UseVisualStyleBackColor = False
+        '
+        'SelectYarnGodownTransfer
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(1234, 581)
+        Me.Controls.Add(Me.BlendPanel1)
+        Me.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.KeyPreview = True
+        Me.Name = "SelectYarnGodownTransfer"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "SelectYarnGodownTransfer"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.BlendPanel1.ResumeLayout(False)
+        CType(Me.gridbilldetails, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gridbill, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CHKEDIT, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.APPROXDATE.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.APPROXDATE, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RepositoryItemCheckEdit1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BlendPanel1 As VbPowerPack.BlendPanel
+    Private WithEvents gridbilldetails As DevExpress.XtraGrid.GridControl
+    Private WithEvents gridbill As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GCHK As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents CHKEDIT As DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit
+    Friend WithEvents GENTRYNO As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GSRNO As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GTYPE As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GMILLNAME As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GQUALITY As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GENDS As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GROLLS As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GWT As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GPROGRAMNO As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GTOTALENDS As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GLENGTH As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents APPROXDATE As DevExpress.XtraEditors.Repository.RepositoryItemDateEdit
+    Friend WithEvents RepositoryItemCheckEdit1 As DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit
+    Friend WithEvents cmdexit As Button
+    Friend WithEvents cmdok As Button
+End Class
---
+++ TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Yarn Module/Yarn InterGodown Transfer/SelectYarnGodownTransfer.vb
@@ -0,0 +1,80 @@
+﻿Imports BL
+
+Public Class SelectYarnGodownTransfer
+    Public DT As New DataTable
+    Public TEMPGODOWNNAME As String
+    Sub fillgrid(ByVal WHERE As String)
+        Try
+            Cursor.Current = Cursors.WaitCursor
+            Dim OBJCMN As New ClsCommon()
+            'Dim DT As DataTable = OBJCMN.SEARCH(" CAST (0 AS BIT) AS CHK , NO, SRNO, TYPE, MILLNAME,YARNQUALITY, ISNULL(ENDS, 0) AS ENDS, ROLLS, WT, PROGRAMNO,PROGRAMSRNO, TOTALENDS, LENGTH", "", "ROLLSTOCK", " AND ROLLSTOCK.GODOWN='" & TEMPGODOWNNAME & "' AND ROLLSTOCK.YEARID = " & YearId)
+            Dim DT As DataTable = OBJCMN.SEARCH("SELECT YARNQUALITY, MILLNAME, DESIGNNO, COLOR, LOTNO, SUM(BAGS) AS BAGS, SUM (WT) AS WT,SUM(CONES) AS CONES,  LRNO ,  CAST(GETDATE() AS DATE) AS LIFTINGDATE,GODOWN  FROM YARNSTOCKVIEW WHERE YEARID = " & YearId & WHERE & " AND (BAGS > 0 OR CONES > 0 OR WT > 0)  GROUP BY YARNQUALITY, MILLNAME, DESIGNNO, COLOR, GODOWN, LOTNO,CATEGORY, LRNO", "", "")
+            gridbilldetails.DataSource = DT
+            If DT.Rows.Count > 0 Then
+                gridbill.FocusedRowHandle = gridbill.RowCount - 1
+                gridbill.TopRowIndex = gridbill.RowCount - 15
+            End If
+
+        Catch ex As Exception
+            If ErrHandle(ex.Message.GetHashCode) = False Then Throw ex
+        Finally
+            Cursor.Current = Cursors.Default
+        End Try
+    End Sub
+    Private Sub cmdok_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdok.Click
+        Try
+            Dim COUNT As Integer = 0
+            For i As Integer = 0 To gridbill.RowCount - 1
+                Dim dtrow As DataRow = gridbill.GetDataRow(i)
+                If Convert.ToBoolean(dtrow("CHK")) = True Then
+                    COUNT = COUNT + 1
+                End If
+            Next
+            If COUNT > 1 Then
+                MsgBox("You Can Select Only One Entry")
+                Exit Sub
+            End If
+
+
+            DT.Columns.Add("YARNQUALITY")
+            DT.Columns.Add("MILLNAME")
+            DT.Columns.Add("DESIGNNO")
+            DT.Columns.Add("COLOR")
+            DT.Columns.Add("LOTNO")
+            DT.Columns.Add("BAGS")
+            DT.Columns.Add("WT")
+            DT.Columns.Add("CONES")
+            DT.Columns.Add("LRNO")
+            DT.Columns.Add("LIFTINGDATE")
+            'DT.Columns.Add("TOTALENDS")
+            'DT.Columns.Add("LENGTH")
+
+            For i As Integer = 0 To gridbill.RowCount - 1
+                Dim dtrow As DataRow = gridbill.GetDataRow(i)
+                If Convert.ToBoolean(dtrow("CHK")) = True Then
+                    DT.Rows.Add(dtrow("YARNQUALITY"), dtrow("MILLNAME"), dtrow("DESIGNNO"), dtrow("COLOR"), dtrow("LOTNO"), dtrow("BAGS"), Val(dtrow("WT")), Val(dtrow("CONES")), dtrow("LRNO"), dtrow("LIFTINGDATE"))
+                End If
+            Next
+            Me.Close()
+
+        Catch ex As Exception
+            If ErrHandle(ex.Message.GetHashCode) = False Then Throw ex
+        End Try
+    End Sub
+
+    Private Sub SelectRolls_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Windows.Forms.Keys.Escape Then
+            Me.Close()
+        ElseIf e.KeyCode = Keys.Enter Then
+            SendKeys.Send("{Tab}")
+        End If
+    End Sub
+
+    Private Sub SelectRolls_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        fillgrid("")
+    End Sub
+
+    Private Sub cmdexit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdexit.Click
+        Me.Close()
+    End Sub
+End Class
