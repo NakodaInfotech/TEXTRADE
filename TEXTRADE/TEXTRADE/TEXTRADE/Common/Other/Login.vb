abc588332f079d6a8cef39b8e4c4d40cb4e77ab0
Chandrish Pal <sales@nakodainfotech.com>
2026-02-21 12:12:59 +0530

    CHAGES FOR SWPL

--- TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
@@ -996,6 +996,7 @@ line1:
                         GoTo LINE1
                     End If
                 ElseIf ClientName = "SWPL" Then
+                    HIDEYARN = False
                     If Now.Date > DateTime.Parse("15.02.2027 00:00") Then
                         Dim DTNEW As DataTable = OBJCMN.Execute_Any_String("UPDATE VERSION SET VERSION_NO='1.0.0000'", "", "")
                         GoTo LINE1
