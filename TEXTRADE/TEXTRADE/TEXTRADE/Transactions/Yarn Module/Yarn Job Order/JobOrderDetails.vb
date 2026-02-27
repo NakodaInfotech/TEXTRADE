adeb079e84602489d46ba4d32ce8cce929c93857
Moin <sales@nakodainfotech.com>
2026-02-27 17:05:04 +0530

    CHABNGES

--- TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Yarn Module/Yarn Job Order/JobOrderDetails.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Yarn Module/Yarn Job Order/JobOrderDetails.vb
@@ -29,7 +29,7 @@ Public Class JobOrderDetails
 
     Private Sub StoreConsumptionDetails_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Try
-            Dim DTROW() As DataRow = USERRIGHTS.Select("FormName = 'STORES'")
+            Dim DTROW() As DataRow = USERRIGHTS.Select("FormName = 'DESIGN MASTER'")
             USERADD = DTROW(0).Item(1)
             USEREDIT = DTROW(0).Item(2)
             USERVIEW = DTROW(0).Item(3)
