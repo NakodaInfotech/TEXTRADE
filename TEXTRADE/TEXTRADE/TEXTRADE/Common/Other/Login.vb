25104090bfdbb202a02a82f2dcf601345bd71d60
Moin <sales@nakodainfotech.com>
2026-03-06 17:44:00 +0530

    CHANGES

--- TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
@@ -224,6 +224,11 @@ line1:
                         Dim DTNEW As DataTable = OBJCMN.Execute_Any_String("UPDATE VERSION SET VERSION_NO='1.0.0000'", "", "")
                         GoTo LINE1
                     End If
+                ElseIf ClientName = "ANKUSH" Then
+                    If Now.Date > DateTime.Parse("15.03.2027 00:00") Then
+                        Dim DTNEW As DataTable = OBJCMN.Execute_Any_String("UPDATE VERSION SET VERSION_NO='1.0.0000'", "", "")
+                        GoTo LINE1
+                    End If
                 ElseIf ClientName = "ANOX" Then
                     DISCONTINUECLIENT = True
                     'HIDESAMPLEMODULE = False
