1aa9f404de9b9f6ce195d70a6334ec6fb1488de4
Moin <sales@nakodainfotech.com>
2026-03-07 17:23:46 +0530

    changes

---
+++ TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Stores/Stores Loan/StoresLoanDetails.vb
@@ -0,0 +1,121 @@
+﻿Imports BL
+Imports System.Windows.Forms
+Public Class StoresLoanDetails
+    Public edit As Boolean
+    Dim TEMPLoanNO As Integer
+    Dim USERADD, USEREDIT, USERVIEW, USERDELETE As Boolean      'USED FOR RIGHT MANAGEMAENT
+
+    Private Sub cmdexit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdexit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub PRequisitionDetails_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        Try
+            If e.KeyCode = Windows.Forms.Keys.Escape Or (e.KeyCode = Keys.X And e.Alt = True) Then
+                Me.Close()
+            ElseIf e.KeyCode = Keys.N And e.Control = True Then
+                showform(False, 0)
+            ElseIf e.KeyCode = Keys.O And e.Alt = True Then
+                cmdok_Click(sender, e)
+            End If
+        Catch ex As Exception
+            If ErrHandle(ex.Message.GetHashCode) = False Then Throw ex
+        End Try
+    End Sub
+
+    Private Sub PRequisitionDetails_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim DTROW() As DataRow
+        DTROW = USERRIGHTS.Select("FormName = 'GRN'")
+        USERADD = DTROW(0).Item(1)
+        USEREDIT = DTROW(0).Item(2)
+        USERVIEW = DTROW(0).Item(3)
+        USERDELETE = DTROW(0).Item(4)
+
+        If USEREDIT = False And USERVIEW = False Then
+            MsgBox("Insufficient Rights")
+            Exit Sub
+        End If
+
+        fillgrid(" and dbo.LOANMASTER.Loan_yearid=" & YearId & " order by dbo.LOANMASTER.Loan_no ")
+    End Sub
+
+    Sub fillgrid(ByVal tepmcondition)
+        Try
+            Dim objclsCMST As New ClsCommonMaster
+            Dim dt As DataTable
+            dt = objclsCMST.search("    LOANMASTER.LOAN_no AS SRNO, LEDGERS.Acc_cmpname AS NAME, LOANMASTER.LOAN_date AS DATE, LOANMASTER.LOAN_TYPE AS LOAN, LOANMASTER_DESC.LOAN_QTY AS QTY, LOANMASTER_DESC.LOAN_AMT AS AMT, ITEMMASTER.ITEM_NAME AS ITEMNAME ", "", "  LOANMASTER INNER JOIN LEDGERS ON LOANMASTER.LOAN_NAMEid = LEDGERS.Acc_id AND LOANMASTER.LOAN_cmpid = LEDGERS.Acc_cmpid AND LOANMASTER.LOAN_locationid = LEDGERS.Acc_locationid AND LOANMASTER.LOAN_yearid = LEDGERS.Acc_yearid INNER JOIN LOANMASTER_DESC ON LOANMASTER.LOAN_no = LOANMASTER_DESC.LOAN_no AND LOANMASTER.LOAN_cmpid = LOANMASTER_DESC.LOAN_cmpid AND LOANMASTER.LOAN_locationid = LOANMASTER_DESC.LOAN_locationid AND LOANMASTER.LOAN_yearid = LOANMASTER_DESC.LOAN_yearid INNER JOIN ITEMMASTER ON ITEM_ID = LOAN_ITEMID", tepmcondition)
+            If dt.Rows.Count > 0 Then
+                gridbilldetails.DataSource = dt
+                gridbill.FocusedRowHandle = gridbill.RowCount - 1
+                gridbill.TopRowIndex = gridbill.RowCount - 15
+            End If
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Sub showform(ByVal editval As Boolean, ByVal LoanNO As Integer)
+        Try
+            If (editval = True And USEREDIT = False And USERVIEW = False) Or (editval = False And USERADD = False) Then
+                MsgBox("Insufficient Rights")
+                Exit Sub
+            End If
+
+            If (editval = False) Or (editval = True And gridbill.RowCount > 0) Then
+                Dim objREQ As New StoresLoan
+                objREQ.MdiParent = MDIMain
+                objREQ.edit = editval
+                objREQ.TEMPloanNO = LoanNO
+                objREQ.Show()
+                'Me.Close()
+            End If
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub ToolStripButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton1.Click
+        Try
+            'If USERADD = False Then
+            '    MsgBox("Insufficient Rights")
+            '    Exit Sub
+            'End If
+            showform(False, 0)
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub gridpayment_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles gridbill.DoubleClick
+        Try
+            showform(True, gridbill.GetFocusedRowCellValue("SRNO"))
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub cmdok_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdok.Click
+        Try
+            showform(True, gridbill.GetFocusedRowCellValue("SRNO"))
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub PrintToolStripButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PrintToolStripButton.Click
+        Try
+
+            Dim PATH As String = Application.StartupPath & "\Loan Details.XLS"
+            Dim opti As New DevExpress.XtraPrinting.XlsExportOptions
+            opti.ShowGridLines = True
+            For Each proc In System.Diagnostics.Process.GetProcessesByName("Excel")
+                proc.Kill()
+            Next
+            opti.SheetName = "Loan Details"
+            gridbill.ExportToXls(PATH, opti)
+            EXCELCMPHEADER(PATH, "Loan Details", gridbill.VisibleColumns.Count + gridbill.GroupCount)
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+End Class
