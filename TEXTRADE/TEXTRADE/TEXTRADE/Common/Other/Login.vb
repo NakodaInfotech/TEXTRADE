0d745dc5eca6dd47baceb1a4c5c13d508fc65534
NakodaInfotech PRAVEEN <sales@nakodainfotech.com>
2026-03-13 12:24:09 +0530

    ABC

--- TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
@@ -1107,7 +1107,7 @@ line1:
                     HIDESAMPLEMODULE = False
                     HIGHVERSION = True
                     HIDEPAYROLL = False
-                    If Now.Date > DateTime.Parse("15.04.2026 00:00") Then
+                    If Now.Date > DateTime.Parse("15.04.2027 00:00") Then
                         Dim DTNEW As DataTable = OBJCMN.Execute_Any_String("UPDATE VERSION SET VERSION_NO='1.0.0000'", "", "")
                         GoTo LINE1
                     End If
