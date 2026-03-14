f76fde010969f7aaa46605c5e5c60ef54a2a1da0
Moin <sales@nakodainfotech.com>
2026-03-14 12:01:13 +0530

    changes

--- TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Yarn Module/Yarn Recd/YarnRecd.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Yarn Module/Yarn Recd/YarnRecd.vb
@@ -2079,7 +2079,7 @@ LINE1:
                 'End If
 
                 'TXTLOTNO.Text = DTROWSAVE("LOTNO")
-                GRIDYARN.Rows.Add(0, DTROWSAVE("ITEMNAME"), "", "", DTROWSAVE("PARTYLOTNO"), "", "", "", "", Format(Val(DTROWSAVE("BAGS")), "0.00"), Format(Val(DTROWSAVE("WT")), "0.00"), 0, DTROWSAVE("BOXNO"), Format(Convert.ToDateTime(YARNDATE.Text).Date, "dd/MM/yyyy"), 0, 0, 0, 0, 0, "", "")
+                GRIDYARN.Rows.Add(0, DTROWSAVE("ITEMNAME"), "", "", DTROWSAVE("PARTYLOTNO"), "", "", "", "", Format(Val(DTROWSAVE("BAGS")), "0.00"), Format(Val(DTROWSAVE("WT")), "0.00"), 0, DTROWSAVE("BOXNO"), Format(Convert.ToDateTime(YARNDATE.Text).Date, "dd/MM/yyyy"), 0, 0, 0, 0, 0, "", 0, "Wt", 0, "")
 
                 DTROWSAVE = DTSAVE.NewRow()
 
