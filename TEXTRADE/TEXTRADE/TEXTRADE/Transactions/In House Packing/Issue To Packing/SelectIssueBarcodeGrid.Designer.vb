cbb04d4fe08da0260d7540f17e100ee43024852f
Chandrish Pal <sales@nakodainfotech.com>
2026-03-20 18:07:30 +0530

    CHAGES

--- TEXTRADE/TEXTRADE/TEXTRADE/Transactions/In House Packing/Issue To Packing/SelectIssueBarcodeGrid.Designer.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Transactions/In House Packing/Issue To Packing/SelectIssueBarcodeGrid.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class SelectIssueBarcodeGrid
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,20 +20,232 @@ Partial Class SelectIssueBarcodeGrid
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.BlendPanel1 = New VbPowerPack.BlendPanel()
+        Me.CMDOK = New System.Windows.Forms.Button()
+        Me.cmdexit = New System.Windows.Forms.Button()
+        Me.gridbilldetails = New DevExpress.XtraGrid.GridControl()
+        Me.gridbill = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GCHK = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.RepositoryItemCheckEdit1 = New DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit()
+        Me.GITEMNAME = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GDESIGNNO = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GCOLOR = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GTOTALPCS = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GUNIT = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GTOTALMTRS = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GBALENO = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GBARCODE = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.BlendPanel1.SuspendLayout()
+        CType(Me.gridbilldetails, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gridbill, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RepositoryItemCheckEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'BlendPanel1
+        '
+        Me.BlendPanel1.AutoSize = True
+        Me.BlendPanel1.Blend = New VbPowerPack.BlendFill(VbPowerPack.BlendStyle.Vertical, System.Drawing.Color.FromArgb(CType(CType(213, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(248, Byte), Integer)), System.Drawing.SystemColors.Window)
+        Me.BlendPanel1.Controls.Add(Me.CMDOK)
+        Me.BlendPanel1.Controls.Add(Me.cmdexit)
+        Me.BlendPanel1.Controls.Add(Me.gridbilldetails)
+        Me.BlendPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.BlendPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.BlendPanel1.Name = "BlendPanel1"
+        Me.BlendPanel1.Size = New System.Drawing.Size(1098, 581)
+        Me.BlendPanel1.TabIndex = 12
+        '
+        'CMDOK
+        '
+        Me.CMDOK.BackColor = System.Drawing.Color.Transparent
+        Me.CMDOK.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.CMDOK.FlatAppearance.BorderSize = 0
+        Me.CMDOK.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CMDOK.ForeColor = System.Drawing.Color.Black
+        Me.CMDOK.Location = New System.Drawing.Point(449, 544)
+        Me.CMDOK.Name = "CMDOK"
+        Me.CMDOK.Size = New System.Drawing.Size(80, 28)
+        Me.CMDOK.TabIndex = 260
+        Me.CMDOK.Text = "&Ok"
+        Me.CMDOK.UseVisualStyleBackColor = False
+        '
+        'cmdexit
+        '
+        Me.cmdexit.BackColor = System.Drawing.Color.Transparent
+        Me.cmdexit.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.cmdexit.FlatAppearance.BorderSize = 0
+        Me.cmdexit.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdexit.ForeColor = System.Drawing.Color.Black
+        Me.cmdexit.Location = New System.Drawing.Point(535, 544)
+        Me.cmdexit.Name = "cmdexit"
+        Me.cmdexit.Size = New System.Drawing.Size(80, 28)
+        Me.cmdexit.TabIndex = 2
+        Me.cmdexit.Text = "E&xit"
+        Me.cmdexit.UseVisualStyleBackColor = False
+        '
+        'gridbilldetails
+        '
+        Me.gridbilldetails.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gridbilldetails.Location = New System.Drawing.Point(12, 12)
+        Me.gridbilldetails.LookAndFeel.UseDefaultLookAndFeel = False
+        Me.gridbilldetails.MainView = Me.gridbill
+        Me.gridbilldetails.Name = "gridbilldetails"
+        Me.gridbilldetails.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.RepositoryItemCheckEdit1})
+        Me.gridbilldetails.Size = New System.Drawing.Size(1074, 525)
+        Me.gridbilldetails.TabIndex = 256
+        Me.gridbilldetails.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gridbill})
+        '
+        'gridbill
+        '
+        Me.gridbill.Appearance.HeaderPanel.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gridbill.Appearance.HeaderPanel.Options.UseFont = True
+        Me.gridbill.Appearance.Row.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gridbill.Appearance.Row.Options.UseFont = True
+        Me.gridbill.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GCHK, Me.GITEMNAME, Me.GDESIGNNO, Me.GCOLOR, Me.GTOTALPCS, Me.GTOTALMTRS, Me.GUNIT, Me.GBALENO, Me.GBARCODE})
+        Me.gridbill.CustomizationFormBounds = New System.Drawing.Rectangle(688, 311, 208, 184)
+        Me.gridbill.GridControl = Me.gridbilldetails
+        Me.gridbill.Name = "gridbill"
+        Me.gridbill.OptionsBehavior.AutoExpandAllGroups = True
+        Me.gridbill.OptionsView.ColumnAutoWidth = False
+        Me.gridbill.OptionsView.GroupFooterShowMode = DevExpress.XtraGrid.Views.Grid.GroupFooterShowMode.VisibleAlways
+        Me.gridbill.OptionsView.ShowAutoFilterRow = True
+        Me.gridbill.OptionsView.ShowFooter = True
+        Me.gridbill.OptionsView.ShowGroupPanel = False
+        '
+        'GCHK
+        '
+        Me.GCHK.Caption = "CHK"
+        Me.GCHK.ColumnEdit = Me.RepositoryItemCheckEdit1
+        Me.GCHK.FieldName = "CHK"
+        Me.GCHK.Name = "GCHK"
+        Me.GCHK.Visible = True
+        Me.GCHK.VisibleIndex = 0
+        Me.GCHK.Width = 50
+        '
+        'RepositoryItemCheckEdit1
+        '
+        Me.RepositoryItemCheckEdit1.AutoHeight = False
+        Me.RepositoryItemCheckEdit1.Name = "RepositoryItemCheckEdit1"
+        Me.RepositoryItemCheckEdit1.NullStyle = DevExpress.XtraEditors.Controls.StyleIndeterminate.Unchecked
+        '
+        'GITEMNAME
+        '
+        Me.GITEMNAME.Caption = "Item Name"
+        Me.GITEMNAME.FieldName = "ITEMNAME"
+        Me.GITEMNAME.Name = "GITEMNAME"
+        Me.GITEMNAME.OptionsColumn.AllowEdit = False
+        Me.GITEMNAME.Visible = True
+        Me.GITEMNAME.VisibleIndex = 1
+        Me.GITEMNAME.Width = 200
+        '
+        'GDESIGNNO
+        '
+        Me.GDESIGNNO.Caption = "Design"
+        Me.GDESIGNNO.FieldName = "DESIGNNO"
+        Me.GDESIGNNO.Name = "GDESIGNNO"
+        Me.GDESIGNNO.OptionsColumn.AllowEdit = False
+        Me.GDESIGNNO.Visible = True
+        Me.GDESIGNNO.VisibleIndex = 2
+        Me.GDESIGNNO.Width = 150
+        '
+        'GCOLOR
+        '
+        Me.GCOLOR.Caption = "Shade"
+        Me.GCOLOR.FieldName = "COLOR"
+        Me.GCOLOR.Name = "GCOLOR"
+        Me.GCOLOR.OptionsColumn.AllowEdit = False
+        Me.GCOLOR.Visible = True
+        Me.GCOLOR.VisibleIndex = 3
+        Me.GCOLOR.Width = 150
+        '
+        'GTOTALPCS
+        '
+        Me.GTOTALPCS.Caption = "Pcs"
+        Me.GTOTALPCS.DisplayFormat.FormatString = "0"
+        Me.GTOTALPCS.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric
+        Me.GTOTALPCS.FieldName = "TOTALPCS"
+        Me.GTOTALPCS.Name = "GTOTALPCS"
+        Me.GTOTALPCS.OptionsColumn.AllowEdit = False
+        Me.GTOTALPCS.Summary.AddRange(New DevExpress.XtraGrid.GridSummaryItem() {New DevExpress.XtraGrid.GridColumnSummaryItem(DevExpress.Data.SummaryItemType.Sum)})
+        Me.GTOTALPCS.Visible = True
+        Me.GTOTALPCS.VisibleIndex = 4
+        Me.GTOTALPCS.Width = 80
+        '
+        'GUNIT
+        '
+        Me.GUNIT.Caption = "Unit"
+        Me.GUNIT.FieldName = "UNIT"
+        Me.GUNIT.Name = "GUNIT"
+        Me.GUNIT.OptionsColumn.AllowEdit = False
+        Me.GUNIT.Visible = True
+        Me.GUNIT.VisibleIndex = 5
+        '
+        'GTOTALMTRS
+        '
+        Me.GTOTALMTRS.Caption = "Mtrs."
+        Me.GTOTALMTRS.DisplayFormat.FormatString = "0.00"
+        Me.GTOTALMTRS.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric
+        Me.GTOTALMTRS.FieldName = "TOTALMTRS"
+        Me.GTOTALMTRS.Name = "GTOTALMTRS"
+        Me.GTOTALMTRS.OptionsColumn.AllowEdit = False
+        Me.GTOTALMTRS.Summary.AddRange(New DevExpress.XtraGrid.GridSummaryItem() {New DevExpress.XtraGrid.GridColumnSummaryItem(DevExpress.Data.SummaryItemType.Sum)})
+        Me.GTOTALMTRS.Visible = True
+        Me.GTOTALMTRS.VisibleIndex = 6
+        Me.GTOTALMTRS.Width = 90
+        '
+        'GBALENO
+        '
+        Me.GBALENO.Caption = "Bale No"
+        Me.GBALENO.FieldName = "BALENO"
+        Me.GBALENO.Name = "GBALENO"
+        Me.GBALENO.OptionsColumn.AllowEdit = False
+        Me.GBALENO.Visible = True
+        Me.GBALENO.VisibleIndex = 7
+        Me.GBALENO.Width = 100
+        '
+        'GBARCODE
+        '
+        Me.GBARCODE.Caption = "Barcode"
+        Me.GBARCODE.FieldName = "BARCODE"
+        Me.GBARCODE.Name = "GBARCODE"
+        Me.GBARCODE.OptionsColumn.AllowEdit = False
+        Me.GBARCODE.Visible = True
+        Me.GBARCODE.VisibleIndex = 8
+        Me.GBARCODE.Width = 140
         '
         'SelectIssueBarcodeGrid
         '
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1098, 581)
+        Me.Controls.Add(Me.BlendPanel1)
         Me.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.KeyPreview = True
         Me.Name = "SelectIssueBarcodeGrid"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Select Issue Barcode Grid"
+        Me.BlendPanel1.ResumeLayout(False)
+        CType(Me.gridbilldetails, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gridbill, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RepositoryItemCheckEdit1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents BlendPanel1 As VbPowerPack.BlendPanel
+    Friend WithEvents CMDOK As Button
+    Friend WithEvents cmdexit As Button
+    Private WithEvents gridbilldetails As DevExpress.XtraGrid.GridControl
+    Private WithEvents gridbill As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GCHK As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents RepositoryItemCheckEdit1 As DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit
+    Friend WithEvents GITEMNAME As DevExpress.XtraGrid.Columns.GridColumn
+    Private WithEvents GDESIGNNO As DevExpress.XtraGrid.Columns.GridColumn
+    Private WithEvents GCOLOR As DevExpress.XtraGrid.Columns.GridColumn
+    Private WithEvents GTOTALPCS As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GUNIT As DevExpress.XtraGrid.Columns.GridColumn
+    Private WithEvents GTOTALMTRS As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GBALENO As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GBARCODE As DevExpress.XtraGrid.Columns.GridColumn
 End Class
