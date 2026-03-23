801145bc90ee408bb15a86092b00039c25f54728
Chandrish Pal <sales@nakodainfotech.com>
2026-03-23 20:02:31 +0530

    CHANGES FOR

--- TEXTRADE/TEXTRADE/BL/Transactions/Yarn Modules/BeamReceivedWarper/ClsBeamReceivedWarper.vb
+++ TEXTRADE/TEXTRADE/BL/Transactions/Yarn Modules/BeamReceivedWarper/ClsBeamReceivedWarper.vb
@@ -35,57 +35,16 @@ Public Class ClsBeamReceivedWarper
                 I = I + 1
                 .Add(New SqlClient.SqlParameter("@MILLNAME", alParaval(I)))
                 I = I + 1
-                .Add(New SqlClient.SqlParameter("@JOBNO", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@REED", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@REEDSPACE", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@CHALLANNO", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@CHALLANDATE", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@TOTALBEAM", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@TOTALMTRS", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@TOTALBEAMWT", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@AVGTAPLINE", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@REMARKS", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@FROMTOBEAM", alParaval(I)))
-                I = I + 1
 
-                .Add(New SqlClient.SqlParameter("@cmpid", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@userid", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@yearid", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@PICS", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@REFNO", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@FROMTYPE", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@TOTALROLLNO", alParaval(I)))
-                I = I + 1
-
-
-                'grid parameters
-
-                .Add(New SqlClient.SqlParameter("@GRIDSRNO", alParaval(I)))
+                .Add(New SqlClient.SqlParameter("@BEAMNAME", alParaval(I)))
                 I = I + 1
                 .Add(New SqlClient.SqlParameter("@BEAMNO", alParaval(I)))
                 I = I + 1
-                .Add(New SqlClient.SqlParameter("@BEAMNAME", alParaval(I)))
+                .Add(New SqlClient.SqlParameter("@TOTALMTRS", alParaval(I)))
                 I = I + 1
                 .Add(New SqlClient.SqlParameter("@ENDS", alParaval(I)))
                 I = I + 1
-                .Add(New SqlClient.SqlParameter("@MTRS", alParaval(I)))
-                I = I + 1
+
                 .Add(New SqlClient.SqlParameter("@GAMANO", alParaval(I)))
                 I = I + 1
                 .Add(New SqlClient.SqlParameter("@SECTION", alParaval(I)))
@@ -95,6 +54,54 @@ Public Class ClsBeamReceivedWarper
                 .Add(New SqlClient.SqlParameter("@BEAMWT", alParaval(I)))
                 I = I + 1
                 .Add(New SqlClient.SqlParameter("@BREAKAGE", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@CHALLANNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@CHALLANDATE", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@TOTALJOBMTRS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@GRIDTOTALMTRS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@AVGTAPLINE", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@REMARKS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@cmpid", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@userid", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@yearid", alParaval(I)))
+                I = I + 1
+
+
+                'grid parameters
+
+                .Add(New SqlClient.SqlParameter("@GRIDSRNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@JOBNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@JOBSRNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@ITEMNAME", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@REED", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@REEDSPACE", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@PICS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@DESC", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@GRIDENDS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@REFNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@JOBMTRS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@BEAMMTRS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@FROMTYPE", alParaval(I)))
                 I = I + 1
                 .Add(New SqlClient.SqlParameter("@GRIDDONE", alParaval(I)))
                 I = I + 1
@@ -127,57 +134,16 @@ Public Class ClsBeamReceivedWarper
                 I = I + 1
                 .Add(New SqlClient.SqlParameter("@MILLNAME", alParaval(I)))
                 I = I + 1
-                .Add(New SqlClient.SqlParameter("@JOBNO", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@REED", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@REEDSPACE", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@CHALLANNO", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@CHALLANDATE", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@TOTALBEAM", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@TOTALMTRS", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@TOTALBEAMWT", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@AVGTAPLINE", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@REMARKS", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@FROMTOBEAM", alParaval(I)))
-                I = I + 1
 
-                .Add(New SqlClient.SqlParameter("@cmpid", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@userid", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@yearid", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@PICS", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@REFNO", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@FROMTYPE", alParaval(I)))
-                I = I + 1
-                .Add(New SqlClient.SqlParameter("@TOTALROLLNO", alParaval(I)))
-                I = I + 1
-
-
-                'grid parameters
-
-                .Add(New SqlClient.SqlParameter("@GRIDSRNO", alParaval(I)))
+                .Add(New SqlClient.SqlParameter("@BEAMNAME", alParaval(I)))
                 I = I + 1
                 .Add(New SqlClient.SqlParameter("@BEAMNO", alParaval(I)))
                 I = I + 1
-                .Add(New SqlClient.SqlParameter("@BEAMNAME", alParaval(I)))
+                .Add(New SqlClient.SqlParameter("@TOTALMTRS", alParaval(I)))
                 I = I + 1
                 .Add(New SqlClient.SqlParameter("@ENDS", alParaval(I)))
                 I = I + 1
-                .Add(New SqlClient.SqlParameter("@MTRS", alParaval(I)))
-                I = I + 1
+
                 .Add(New SqlClient.SqlParameter("@GAMANO", alParaval(I)))
                 I = I + 1
                 .Add(New SqlClient.SqlParameter("@SECTION", alParaval(I)))
@@ -187,6 +153,54 @@ Public Class ClsBeamReceivedWarper
                 .Add(New SqlClient.SqlParameter("@BEAMWT", alParaval(I)))
                 I = I + 1
                 .Add(New SqlClient.SqlParameter("@BREAKAGE", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@CHALLANNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@CHALLANDATE", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@TOTALJOBMTRS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@GRIDTOTALMTRS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@AVGTAPLINE", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@REMARKS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@cmpid", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@userid", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@yearid", alParaval(I)))
+                I = I + 1
+
+
+                'grid parameters
+
+                .Add(New SqlClient.SqlParameter("@GRIDSRNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@JOBNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@JOBSRNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@ITEMNAME", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@REED", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@REEDSPACE", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@PICS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@DESC", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@GRIDENDS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@REFNO", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@JOBMTRS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@BEAMMTRS", alParaval(I)))
+                I = I + 1
+                .Add(New SqlClient.SqlParameter("@FROMTYPE", alParaval(I)))
                 I = I + 1
                 .Add(New SqlClient.SqlParameter("@GRIDDONE", alParaval(I)))
                 I = I + 1
