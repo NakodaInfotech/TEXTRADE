96101f08a6408760582e1668ed7a6a6f6a94801f
NakodaInfotech PRAVEEN <sales@nakodainfotech.com>
2026-04-06 14:01:24 +0530

    ABC

--- TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
@@ -347,7 +347,7 @@ line1:
                         GoTo LINE1
                     End If
                 ElseIf ClientName = "DETLINE" Then
-                    If Now.Date > DateTime.Parse("15.04.2026 00:00") Then
+                    If Now.Date > DateTime.Parse("15.04.2027 00:00") Then
                         Dim DTNEW As DataTable = OBJCMN.Execute_Any_String("UPDATE VERSION SET VERSION_NO='1.0.0000'", "", "")
                         GoTo LINE1
                     End If
