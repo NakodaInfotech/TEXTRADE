a0c3bdeb8c2674419d297e08c2c42e5daf94c86a
NakodaInfotech PRAVEEN <sales@nakodainfotech.com>
2026-04-09 17:45:11 +0530

    ABC

--- TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
@@ -374,7 +374,7 @@ line1:
                     End If
                 ElseIf ClientName = "DRDRAPES" Then
                     ALLOWDIGITALSIGN = True
-                    If Now.Date > DateTime.Parse("15.04.2026 00:00") Then
+                    If Now.Date > DateTime.Parse("15.04.2027 00:00") Then
                         Dim DTNEW As DataTable = OBJCMN.Execute_Any_String("UPDATE VERSION SET VERSION_NO='1.0.0000'", "", "")
                         GoTo LINE1
                     End If
