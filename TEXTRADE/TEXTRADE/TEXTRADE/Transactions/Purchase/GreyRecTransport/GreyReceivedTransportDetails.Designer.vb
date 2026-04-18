c0dc086cedfdbfa684855aaa8d65c54180750e58
Moin <sales@nakodainfotech.com>
2026-04-18 14:19:57 +0530

    CHANGES

--- TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Purchase/GreyRecTransport/GreyReceivedTransportDetails.Designer.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Purchase/GreyRecTransport/GreyReceivedTransportDetails.Designer.vb
@@ -62,6 +62,11 @@ Partial Class GreyReceivedTransportDetails
         Me.GREMARKS = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.GAGENT = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.GCRDAYS = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GGODOWN = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GBARCODE = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GCREATED = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GMODIFIED = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GMODIFIEDBY = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
         Me.TOOLADDNEW = New System.Windows.Forms.ToolStripButton()
         Me.toolStripSeparator = New System.Windows.Forms.ToolStripSeparator()
@@ -75,11 +80,6 @@ Partial Class GreyReceivedTransportDetails
         Me.PRINTDOC = New System.Drawing.Printing.PrintDocument()
         Me.imageList1 = New System.Windows.Forms.ImageList(Me.components)
         Me.PRINTDIALOG = New System.Windows.Forms.PrintDialog()
-        Me.GGODOWN = New DevExpress.XtraGrid.Columns.GridColumn()
-        Me.GBAROCDE = New DevExpress.XtraGrid.Columns.GridColumn()
-        Me.GCREATED = New DevExpress.XtraGrid.Columns.GridColumn()
-        Me.GMODIFIED = New DevExpress.XtraGrid.Columns.GridColumn()
-        Me.GMODIFIEDBY = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.BlendPanel1.SuspendLayout()
         CType(Me.gridbilldetails, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.gridbill, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -213,7 +213,7 @@ Partial Class GreyReceivedTransportDetails
         '
         Me.gridbill.Appearance.Row.Font = New System.Drawing.Font("Calibri", 9.0!)
         Me.gridbill.Appearance.Row.Options.UseFont = True
-        Me.gridbill.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.gsrno, Me.gdate, Me.GNAME, Me.GPONO, Me.GPODATE, Me.GCHALLAN, Me.GCHALLANDATE, Me.GLRNO, Me.GLRDATE, Me.GITEMNAME, Me.GQUALITY, Me.GBALENO, Me.GDESIGNNO, Me.GSHADE, Me.GQTY, Me.GQTYUNIT, Me.GCUT, Me.GMTRS, Me.GISSUEMTRS, Me.GBALMTRS, Me.GWT, Me.GRATE, Me.GAMT, Me.GTRANSPORT, Me.GREMARKS, Me.GAGENT, Me.GCRDAYS, Me.GGODOWN, Me.GBAROCDE, Me.GCREATED, Me.GMODIFIED, Me.GMODIFIEDBY})
+        Me.gridbill.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.gsrno, Me.gdate, Me.GNAME, Me.GPONO, Me.GPODATE, Me.GCHALLAN, Me.GCHALLANDATE, Me.GLRNO, Me.GLRDATE, Me.GITEMNAME, Me.GQUALITY, Me.GBALENO, Me.GDESIGNNO, Me.GSHADE, Me.GQTY, Me.GQTYUNIT, Me.GCUT, Me.GMTRS, Me.GISSUEMTRS, Me.GBALMTRS, Me.GWT, Me.GRATE, Me.GAMT, Me.GTRANSPORT, Me.GREMARKS, Me.GAGENT, Me.GCRDAYS, Me.GGODOWN, Me.GBARCODE, Me.GCREATED, Me.GMODIFIED, Me.GMODIFIEDBY})
         Me.gridbill.CustomizationFormBounds = New System.Drawing.Rectangle(688, 311, 208, 184)
         Me.gridbill.GridControl = Me.gridbilldetails
         Me.gridbill.Name = "gridbill"
@@ -462,6 +462,60 @@ Partial Class GreyReceivedTransportDetails
         Me.GCRDAYS.Visible = True
         Me.GCRDAYS.VisibleIndex = 19
         '
+        'GGODOWN
+        '
+        Me.GGODOWN.Caption = "Godown"
+        Me.GGODOWN.FieldName = "GODOWN"
+        Me.GGODOWN.Name = "GGODOWN"
+        Me.GGODOWN.OptionsColumn.AllowEdit = False
+        Me.GGODOWN.Visible = True
+        Me.GGODOWN.VisibleIndex = 20
+        Me.GGODOWN.Width = 150
+        '
+        'GBARCODE
+        '
+        Me.GBARCODE.Caption = "Barcode"
+        Me.GBARCODE.FieldName = "BARCODE"
+        Me.GBARCODE.Name = "GBARCODE"
+        Me.GBARCODE.OptionsColumn.AllowEdit = False
+        Me.GBARCODE.Visible = True
+        Me.GBARCODE.VisibleIndex = 21
+        Me.GBARCODE.Width = 100
+        '
+        'GCREATED
+        '
+        Me.GCREATED.Caption = "Created"
+        Me.GCREATED.DisplayFormat.FormatString = "dd/MM/yyyy"
+        Me.GCREATED.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.GCREATED.FieldName = "CREATED"
+        Me.GCREATED.Name = "GCREATED"
+        Me.GCREATED.OptionsColumn.AllowEdit = False
+        Me.GCREATED.Visible = True
+        Me.GCREATED.VisibleIndex = 22
+        Me.GCREATED.Width = 80
+        '
+        'GMODIFIED
+        '
+        Me.GMODIFIED.Caption = "Modified"
+        Me.GMODIFIED.DisplayFormat.FormatString = "dd/MM/yyyy"
+        Me.GMODIFIED.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.GMODIFIED.FieldName = "MODIFIED"
+        Me.GMODIFIED.Name = "GMODIFIED"
+        Me.GMODIFIED.OptionsColumn.AllowEdit = False
+        Me.GMODIFIED.Visible = True
+        Me.GMODIFIED.VisibleIndex = 23
+        Me.GMODIFIED.Width = 80
+        '
+        'GMODIFIEDBY
+        '
+        Me.GMODIFIEDBY.Caption = "Modified By"
+        Me.GMODIFIEDBY.FieldName = "MODIFIEDBY"
+        Me.GMODIFIEDBY.Name = "GMODIFIEDBY"
+        Me.GMODIFIEDBY.OptionsColumn.AllowEdit = False
+        Me.GMODIFIEDBY.Visible = True
+        Me.GMODIFIEDBY.VisibleIndex = 24
+        Me.GMODIFIEDBY.Width = 100
+        '
         'ToolStrip1
         '
         Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TOOLADDNEW, Me.toolStripSeparator, Me.TOOLREFRESH, Me.TOOLEXCEL, Me.TOOLMAIL, Me.TOOLWHATSAPP, Me.PrintToolStripButton, Me.ToolStripSeparator1})
@@ -560,51 +614,6 @@ Partial Class GreyReceivedTransportDetails
         Me.PRINTDIALOG.ShowHelp = True
         Me.PRINTDIALOG.UseEXDialog = True
         '
-        'GGODOWN
-        '
-        Me.GGODOWN.Caption = "Godown"
-        Me.GGODOWN.FieldName = "GODOWN"
-        Me.GGODOWN.Name = "GGODOWN"
-        Me.GGODOWN.Visible = True
-        Me.GGODOWN.VisibleIndex = 20
-        Me.GGODOWN.Width = 150
-        '
-        'GBAROCDE
-        '
-        Me.GBAROCDE.Caption = "Barcode"
-        Me.GBAROCDE.FieldName = "BAROCDE"
-        Me.GBAROCDE.Name = "GBAROCDE"
-        Me.GBAROCDE.Visible = True
-        Me.GBAROCDE.VisibleIndex = 21
-        Me.GBAROCDE.Width = 100
-        '
-        'GCREATED
-        '
-        Me.GCREATED.Caption = "Created"
-        Me.GCREATED.FieldName = "CREATED"
-        Me.GCREATED.Name = "GCREATED"
-        Me.GCREATED.Visible = True
-        Me.GCREATED.VisibleIndex = 22
-        Me.GCREATED.Width = 80
-        '
-        'GMODIFIED
-        '
-        Me.GMODIFIED.Caption = "Modified"
-        Me.GMODIFIED.FieldName = "MODIFIED"
-        Me.GMODIFIED.Name = "GMODIFIED"
-        Me.GMODIFIED.Visible = True
-        Me.GMODIFIED.VisibleIndex = 23
-        Me.GMODIFIED.Width = 80
-        '
-        'GMODIFIEDBY
-        '
-        Me.GMODIFIEDBY.Caption = "Modified By"
-        Me.GMODIFIEDBY.FieldName = "MODIFIEDBY"
-        Me.GMODIFIEDBY.Name = "GMODIFIEDBY"
-        Me.GMODIFIEDBY.Visible = True
-        Me.GMODIFIEDBY.VisibleIndex = 24
-        Me.GMODIFIEDBY.Width = 100
-        '
         'GreyReceivedTransportDetails
         '
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
@@ -678,7 +687,7 @@ Partial Class GreyReceivedTransportDetails
     Friend WithEvents GAGENT As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents GCRDAYS As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents GGODOWN As DevExpress.XtraGrid.Columns.GridColumn
-    Friend WithEvents GBAROCDE As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GBARCODE As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents GCREATED As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents GMODIFIED As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents GMODIFIEDBY As DevExpress.XtraGrid.Columns.GridColumn
