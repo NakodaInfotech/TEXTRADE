d7616d7f535031a526b400af43b20d53fb9ef234
Gulkit <sales@nakodainfotech.com>
2026-04-20 14:57:52 +0530

    ABHEE CHANGES COMPLAINT REGISTER

--- TEXTRADE/TEXTRADE/TEXTRADE/Reports/Grid Reports/Other Reports/ComplaintRegisterReport.Designer.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Reports/Grid Reports/Other Reports/ComplaintRegisterReport.Designer.vb
@@ -22,9 +22,256 @@ Partial Class ComplaintRegisterReport
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "ComplaintRegisterReport"
+        Me.BlendPanel1 = New VbPowerPack.BlendPanel()
+        Me.CMDREFRESH = New System.Windows.Forms.Button()
+        Me.CMDPRINT = New System.Windows.Forms.Button()
+        Me.cmdcancel = New System.Windows.Forms.Button()
+        Me.gridbilldetails = New DevExpress.XtraGrid.GridControl()
+        Me.gridbill = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GBILLNO = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GREGNAME = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GDATE = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GSELLERNAME = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GBUYERNAME = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GPARTYBILLNO = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GITEMNAME = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GTRANSNAME = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GLRNO = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GCOMPLAINT = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GCOMPLAINTBY = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GCOMPLAINTDATE = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GTYPE = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.BlendPanel1.SuspendLayout()
+        CType(Me.gridbilldetails, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gridbill, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BlendPanel1
+        '
+        Me.BlendPanel1.Blend = New VbPowerPack.BlendFill(VbPowerPack.BlendStyle.Vertical, System.Drawing.Color.FromArgb(CType(CType(213, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(248, Byte), Integer)), System.Drawing.SystemColors.Window)
+        Me.BlendPanel1.Controls.Add(Me.CMDREFRESH)
+        Me.BlendPanel1.Controls.Add(Me.CMDPRINT)
+        Me.BlendPanel1.Controls.Add(Me.cmdcancel)
+        Me.BlendPanel1.Controls.Add(Me.gridbilldetails)
+        Me.BlendPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.BlendPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.BlendPanel1.Name = "BlendPanel1"
+        Me.BlendPanel1.Size = New System.Drawing.Size(1284, 581)
+        Me.BlendPanel1.TabIndex = 2
+        '
+        'CMDREFRESH
+        '
+        Me.CMDREFRESH.Location = New System.Drawing.Point(575, 545)
+        Me.CMDREFRESH.Name = "CMDREFRESH"
+        Me.CMDREFRESH.Size = New System.Drawing.Size(80, 28)
+        Me.CMDREFRESH.TabIndex = 3
+        Me.CMDREFRESH.Text = "&Refresh"
+        Me.CMDREFRESH.UseVisualStyleBackColor = True
+        '
+        'CMDPRINT
+        '
+        Me.CMDPRINT.BackColor = System.Drawing.Color.Transparent
+        Me.CMDPRINT.BackgroundImage = Global.TEXTRADE.My.Resources.Resources.Excel_icon
+        Me.CMDPRINT.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.CMDPRINT.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.CMDPRINT.FlatAppearance.BorderSize = 0
+        Me.CMDPRINT.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.CMDPRINT.Font = New System.Drawing.Font("Calibri", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CMDPRINT.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.CMDPRINT.Location = New System.Drawing.Point(544, 545)
+        Me.CMDPRINT.Name = "CMDPRINT"
+        Me.CMDPRINT.Size = New System.Drawing.Size(25, 21)
+        Me.CMDPRINT.TabIndex = 4
+        Me.CMDPRINT.UseVisualStyleBackColor = False
+        '
+        'cmdcancel
+        '
+        Me.cmdcancel.BackColor = System.Drawing.Color.Transparent
+        Me.cmdcancel.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.cmdcancel.FlatAppearance.BorderSize = 0
+        Me.cmdcancel.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdcancel.ForeColor = System.Drawing.Color.Black
+        Me.cmdcancel.Location = New System.Drawing.Point(661, 545)
+        Me.cmdcancel.Name = "cmdcancel"
+        Me.cmdcancel.Size = New System.Drawing.Size(80, 28)
+        Me.cmdcancel.TabIndex = 5
+        Me.cmdcancel.Text = "&Exit"
+        Me.cmdcancel.UseVisualStyleBackColor = False
+        '
+        'gridbilldetails
+        '
+        Me.gridbilldetails.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gridbilldetails.Location = New System.Drawing.Point(15, 12)
+        Me.gridbilldetails.LookAndFeel.UseDefaultLookAndFeel = False
+        Me.gridbilldetails.MainView = Me.gridbill
+        Me.gridbilldetails.Name = "gridbilldetails"
+        Me.gridbilldetails.Size = New System.Drawing.Size(1254, 527)
+        Me.gridbilldetails.TabIndex = 5
+        Me.gridbilldetails.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gridbill})
+        '
+        'gridbill
+        '
+        Me.gridbill.Appearance.Row.Font = New System.Drawing.Font("Calibri", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gridbill.Appearance.Row.Options.UseFont = True
+        Me.gridbill.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GBILLNO, Me.GREGNAME, Me.GDATE, Me.GSELLERNAME, Me.GBUYERNAME, Me.GPARTYBILLNO, Me.GITEMNAME, Me.GTRANSNAME, Me.GLRNO, Me.GCOMPLAINT, Me.GCOMPLAINTBY, Me.GCOMPLAINTDATE, Me.GTYPE})
+        Me.gridbill.GridControl = Me.gridbilldetails
+        Me.gridbill.Name = "gridbill"
+        Me.gridbill.OptionsBehavior.AllowIncrementalSearch = True
+        Me.gridbill.OptionsBehavior.AutoExpandAllGroups = True
+        Me.gridbill.OptionsBehavior.Editable = False
+        Me.gridbill.OptionsView.ColumnAutoWidth = False
+        Me.gridbill.OptionsView.GroupFooterShowMode = DevExpress.XtraGrid.Views.Grid.GroupFooterShowMode.VisibleAlways
+        Me.gridbill.OptionsView.ShowAutoFilterRow = True
+        Me.gridbill.OptionsView.ShowFooter = True
+        Me.gridbill.OptionsView.ShowGroupPanel = False
+        '
+        'GBILLNO
+        '
+        Me.GBILLNO.Caption = "Bill No"
+        Me.GBILLNO.FieldName = "BILLNO"
+        Me.GBILLNO.Name = "GBILLNO"
+        Me.GBILLNO.Visible = True
+        Me.GBILLNO.VisibleIndex = 0
+        '
+        'GREGNAME
+        '
+        Me.GREGNAME.Caption = "Reg Name"
+        Me.GREGNAME.FieldName = "REGNAME"
+        Me.GREGNAME.Name = "GREGNAME"
+        '
+        'GDATE
+        '
+        Me.GDATE.Caption = "Date"
+        Me.GDATE.DisplayFormat.FormatString = "dd/MM/yyyy"
+        Me.GDATE.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.GDATE.FieldName = "DATE"
+        Me.GDATE.Name = "GDATE"
+        Me.GDATE.Visible = True
+        Me.GDATE.VisibleIndex = 1
+        Me.GDATE.Width = 80
+        '
+        'GSELLERNAME
+        '
+        Me.GSELLERNAME.Caption = "Seller Name"
+        Me.GSELLERNAME.FieldName = "SELLERNAME"
+        Me.GSELLERNAME.Name = "GSELLERNAME"
+        Me.GSELLERNAME.Visible = True
+        Me.GSELLERNAME.VisibleIndex = 2
+        Me.GSELLERNAME.Width = 250
+        '
+        'GBUYERNAME
+        '
+        Me.GBUYERNAME.Caption = "Buer Name"
+        Me.GBUYERNAME.FieldName = "BUYERNAME"
+        Me.GBUYERNAME.Name = "GBUYERNAME"
+        Me.GBUYERNAME.Visible = True
+        Me.GBUYERNAME.VisibleIndex = 3
+        Me.GBUYERNAME.Width = 250
+        '
+        'GPARTYBILLNO
+        '
+        Me.GPARTYBILLNO.Caption = "Party Bill No"
+        Me.GPARTYBILLNO.FieldName = "PARTYBILLNO"
+        Me.GPARTYBILLNO.Name = "GPARTYBILLNO"
+        Me.GPARTYBILLNO.Visible = True
+        Me.GPARTYBILLNO.VisibleIndex = 4
+        Me.GPARTYBILLNO.Width = 100
+        '
+        'GITEMNAME
+        '
+        Me.GITEMNAME.Caption = "Item Name"
+        Me.GITEMNAME.FieldName = "ITEMNAME"
+        Me.GITEMNAME.Name = "GITEMNAME"
+        Me.GITEMNAME.Visible = True
+        Me.GITEMNAME.VisibleIndex = 5
+        Me.GITEMNAME.Width = 130
+        '
+        'GTRANSNAME
+        '
+        Me.GTRANSNAME.Caption = "Transport Name"
+        Me.GTRANSNAME.FieldName = "TRANSNAME"
+        Me.GTRANSNAME.Name = "GTRANSNAME"
+        Me.GTRANSNAME.Visible = True
+        Me.GTRANSNAME.VisibleIndex = 6
+        Me.GTRANSNAME.Width = 250
+        '
+        'GLRNO
+        '
+        Me.GLRNO.Caption = "LR No"
+        Me.GLRNO.FieldName = "LRNO"
+        Me.GLRNO.Name = "GLRNO"
+        Me.GLRNO.Visible = True
+        Me.GLRNO.VisibleIndex = 7
+        Me.GLRNO.Width = 100
+        '
+        'GCOMPLAINT
+        '
+        Me.GCOMPLAINT.Caption = "Complaint"
+        Me.GCOMPLAINT.FieldName = "COMPLAINT"
+        Me.GCOMPLAINT.Name = "GCOMPLAINT"
+        Me.GCOMPLAINT.Visible = True
+        Me.GCOMPLAINT.VisibleIndex = 8
+        Me.GCOMPLAINT.Width = 200
+        '
+        'GCOMPLAINTBY
+        '
+        Me.GCOMPLAINTBY.Caption = "Complaint By"
+        Me.GCOMPLAINTBY.FieldName = "COMPLAINTBY"
+        Me.GCOMPLAINTBY.Name = "GCOMPLAINTBY"
+        Me.GCOMPLAINTBY.Visible = True
+        Me.GCOMPLAINTBY.VisibleIndex = 9
+        '
+        'GCOMPLAINTDATE
+        '
+        Me.GCOMPLAINTDATE.Caption = "Com Dt"
+        Me.GCOMPLAINTDATE.FieldName = "COMPLAINTDATE"
+        Me.GCOMPLAINTDATE.Name = "GCOMPLAINTDATE"
+        Me.GCOMPLAINTDATE.Visible = True
+        Me.GCOMPLAINTDATE.VisibleIndex = 10
+        '
+        'GTYPE
+        '
+        Me.GTYPE.Caption = "Type"
+        Me.GTYPE.FieldName = "TYPE"
+        Me.GTYPE.Name = "GTYPE"
+        Me.GTYPE.Visible = True
+        Me.GTYPE.VisibleIndex = 11
+        '
+        'ComplaintRegisterReport
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(1284, 581)
+        Me.Controls.Add(Me.BlendPanel1)
+        Me.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.KeyPreview = True
+        Me.Name = "ComplaintRegisterReport"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "Complaint Register"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.BlendPanel1.ResumeLayout(False)
+        CType(Me.gridbilldetails, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gridbill, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents BlendPanel1 As VbPowerPack.BlendPanel
+    Friend WithEvents CMDREFRESH As Button
+    Friend WithEvents CMDPRINT As Button
+    Friend WithEvents cmdcancel As Button
+    Private WithEvents gridbilldetails As DevExpress.XtraGrid.GridControl
+    Private WithEvents gridbill As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GBILLNO As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GREGNAME As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GDATE As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GSELLERNAME As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GBUYERNAME As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GPARTYBILLNO As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GITEMNAME As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GTRANSNAME As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GLRNO As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GCOMPLAINT As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GCOMPLAINTBY As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GCOMPLAINTDATE As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GTYPE As DevExpress.XtraGrid.Columns.GridColumn
 End Class
--- TEXTRADE/TEXTRADE/TEXTRADE/Reports/Grid Reports/Other Reports/ComplaintRegisterReport.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Reports/Grid Reports/Other Reports/ComplaintRegisterReport.vb
@@ -1,3 +1,67 @@
-﻿Public Class ComplaintRegisterReport
+﻿
+Imports BL
+
+Public Class ComplaintRegisterReport
+
+    Private Sub ComplaintRegisterReport_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Try
+            If e.KeyCode = Windows.Forms.Keys.Escape Or (e.KeyCode = Keys.X And e.Alt = True) Then
+                Me.Close()
+            End If
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Sub FILLGRID()
+        Try
+            Dim OBJCMN As New ClsCommon
+            Dim DT As DataTable = OBJCMN.SEARCH("*", "", "COMPLAINTREGISTERVIEW", " AND YEARID = " & YearId)
+            gridbilldetails.DataSource = DT
+            If DT.Rows.Count > 0 Then gridbill.FocusedRowHandle = gridbill.RowCount - 1
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub cmdcancel_Click(sender As Object, e As EventArgs) Handles cmdcancel.Click
+        Me.Close()
+    End Sub
+
+    Private Sub CMDPRINT_Click(sender As Object, e As EventArgs) Handles CMDPRINT.Click
+        Try
+            Dim PATH As String = "" = ""
+            If FileIO.FileSystem.FileExists(PATH) = True Then FileIO.FileSystem.DeleteFile(PATH)
+            PATH = Application.StartupPath & "\Complaint Register.XLS"
+
+            Dim opti As New DevExpress.XtraPrinting.XlsExportOptions
+            opti.ShowGridLines = True
+
+            Dim PERIOD As String = AccFrom & " - " & AccTo
+
+            opti.SheetName = "Complaint Register"
+            gridbill.ExportToXls(PATH, opti)
+            EXCELCMPHEADER(PATH, "Complaint Register", gridbill.VisibleColumns.Count + gridbill.GroupCount, "", PERIOD)
+
+        Catch ex As Exception
+            MsgBox("Complaint Register Excel File is Open, Please Close the File first then try to Export", MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    Private Sub ComplaintRegisterReport_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Try
+            FILLGRID()
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub CMDREFRESH_Click(sender As Object, e As EventArgs) Handles CMDREFRESH.Click
+        Try
+            FILLGRID()
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
 
 End Class
