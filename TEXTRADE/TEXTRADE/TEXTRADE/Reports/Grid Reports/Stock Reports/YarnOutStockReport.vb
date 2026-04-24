2352adb68ef45ec19f6dad48c4a4b651d9e4180e
Moin <sales@nakodainfotech.com>
2026-04-24 16:40:55 +0530

    CHANGES

--- TEXTRADE/TEXTRADE/TEXTRADE/Reports/Grid Reports/Stock Reports/YarnOutStockReport.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Reports/Grid Reports/Stock Reports/YarnOutStockReport.vb
@@ -1,3 +1,102 @@
-﻿Public Class YarnOutStockReport
+﻿Imports BL
+Imports DevExpress.XtraGrid.Views.Grid
+Public Class YarnOutStockReport
+    Dim USERADD, USEREDIT, USERVIEW, USERDELETE As Boolean      'USED FOR RIGHT MANAGEMAENT
+    Public FRMSTRING As String
 
+    Private Sub cmdexit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdexit.Click
+        Try
+            Me.Close()
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub Opening_Stock_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        Try
+            If e.KeyCode = Windows.Forms.Keys.Escape Then
+                Me.Close()
+            End If
+        Catch ex As Exception
+            If ErrHandle(ex.Message.GetHashCode) = False Then Throw ex
+        End Try
+    End Sub
+
+    Private Sub Opening_Stock_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Try
+            Dim DTROW() As DataRow
+            DTROW = USERRIGHTS.Select("FormName = 'OPENING'")
+            USERADD = DTROW(0).Item(1)
+            USEREDIT = DTROW(0).Item(2)
+            USERVIEW = DTROW(0).Item(3)
+            USERDELETE = DTROW(0).Item(4)
+
+            If USEREDIT = False And USERVIEW = False Then
+                MsgBox("Insufficient Rights")
+                Exit Sub
+            End If
+            FILLGRID()
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub CMDREFRESH_Click(sender As Object, e As EventArgs) Handles CMDREFRESH.Click
+        Try
+            FILLGRID()
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub gridbill_RowStyle(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Grid.RowStyleEventArgs) Handles gridbill.RowStyle
+        Try
+            If e.RowHandle >= 0 Then
+                Dim View As GridView = sender
+                If View.GetRowCellDisplayText(e.RowHandle, View.Columns("DONE")) = "Checked" Then
+                    e.Appearance.Font = New System.Drawing.Font("CALIBRI", 9.0F, System.Drawing.FontStyle.Bold)
+                    e.Appearance.BackColor = Color.Yellow
+                End If
+            End If
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Sub FILLGRID()
+        Try
+            Dim OBJCMN As New ClsCommonMaster
+            Dim DT As New DataTable
+            'If FRMSTRING = "GREY" Then
+            '    DT = OBJCMN.search(" BARCODE, TYPE, PIECETYPE, ITEMNAME AS ITEM, QUALITY, DESIGNNO AS DESIGN, JOBBERNAME AS TONAME, COLOR, GODOWN, MTRS, FROMNO, UNIT, BALENO ", "", "  OUTGREYBARCODESTOCK ", " AND YEARID = " & YearId)
+            'Else
+            DT = OBJCMN.search(" BARCODE, TYPE, PIECETYPE, YARNQUALITY AS YARNQUALITY, MILLNAME, DESIGNNO AS DESIGN, JOBBERNAME , COLOR, GODOWN,BAGS, WT, FROMNO, FROMSRNO,CONES,  LRNO ", "", "  OUTYARNBARCODESTOCK ", " AND YEARID = " & YearId)
+            'End If
+            gridbilldetails.DataSource = DT
+            If DT.Rows.Count > 0 Then
+                gridbill.FocusedRowHandle = gridbill.RowCount - 1
+                gridbill.TopRowIndex = gridbill.RowCount - 15
+            End If
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub PrintToolStripButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PrintToolStripButton.Click
+        Try
+            Dim PATH As String = ""
+            If FileIO.FileSystem.FileExists(PATH) = True Then FileIO.FileSystem.DeleteFile(PATH)
+            PATH = Application.StartupPath & "\Yarn Out Stock Details.XLS"
+
+            Dim opti As New DevExpress.XtraPrinting.XlsExportOptions
+            opti.ShowGridLines = True
+            Dim PERIOD As String = AccFrom & " - " & AccTo
+
+            opti.SheetName = "Yarn Out Stock Details"
+            gridbill.ExportToXls(PATH, opti)
+            EXCELCMPHEADER(PATH, "Yarn Out Stock Details", gridbill.VisibleColumns.Count + gridbill.GroupCount, "", PERIOD)
+        Catch ex As Exception
+            MsgBox("Yarn Out Stock Details Excel File is Open, Please Close the File first then try to Export", MsgBoxStyle.Critical)
+        End Try
+    End Sub
 End Class
