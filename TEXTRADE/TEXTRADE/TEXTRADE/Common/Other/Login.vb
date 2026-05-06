a82d6e08c224dc1349cdfd7e32673e54ff496e0b
NakodaInfotech <sales@nakodainfotech.com>
2026-05-06 11:46:29 +0530

    SYC CHANGES

--- TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
@@ -973,7 +973,8 @@ line1:
                         GoTo LINE1
                     End If
                 ElseIf ClientName = "SYC" Then 'MADHAV KATKORIA
-                    If Now.Date > DateTime.Parse("15.05.2026 00:00") Then
+                    HIDEACCOUNTSEXCEPTINVOICE = True
+                    If Now.Date > DateTime.Parse("15.05.2027 00:00") Then
                         Dim DTNEW As DataTable = OBJCMN.Execute_Any_String("UPDATE VERSION SET VERSION_NO='1.0.0000', VERSION_PCNAME='" & pcName & "'", "", "")
                         GoTo LINE1
                     End If
