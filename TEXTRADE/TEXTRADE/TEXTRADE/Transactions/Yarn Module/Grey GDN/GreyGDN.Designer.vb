92b4bcd3b26d421d01a800f8b1abd2a6b6c8f93a
NakodaInfotech PRAVEEN <sales@nakodainfotech.com>
2026-05-28 14:41:02 +0530

    ABC

--- TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Yarn Module/Grey GDN/GreyGDN.Designer.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Transactions/Yarn Module/Grey GDN/GreyGDN.Designer.vb
@@ -836,6 +836,7 @@ Partial Class GreyGDN
         Me.CMDSELECTSTOCK.TabIndex = 18
         Me.CMDSELECTSTOCK.Text = "Select S&tock"
         Me.CMDSELECTSTOCK.UseVisualStyleBackColor = False
+        Me.CMDSELECTSTOCK.Visible = False
         '
         'LBLGODOWN
         '
