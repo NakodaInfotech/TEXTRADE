c6a73947064b9a780bb9f384a5e13e0a261e4d42
NakodaInfotech <sales@nakodainfotech.com>
2026-06-12 17:57:23 +0530

    KENCOT CHANGES

--- TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
+++ TEXTRADE/TEXTRADE/TEXTRADE/Common/Other/Login.vb
@@ -410,6 +410,7 @@ line1:
                 ElseIf ClientName = "KENCOT" Then
                     HIDESAMPLEMODULE = False
                     HIDEDYEINGPROGRAM = False
+                    HIDECATALOG = False
                     If Now.Date > DateTime.Parse("15.04.2027 00:00") Then
                         Dim DTNEW As DataTable = OBJCMN.Execute_Any_String("UPDATE VERSION SET VERSION_NO='1.0.0000', VERSION_PCNAME='" & pcName & "'", "", "")
                         GoTo LINE1
